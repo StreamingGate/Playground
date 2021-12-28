c02789f8bd394904f36b40fa0e26ec4f40ddc140
chaehui seo <sch0991@naver.com>
2021-12-28 17:31:51 +0900

    chore : add xcode project file

---
+++ src/ios/SG_YouTube/SG_YouTube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SG_YouTube
+//
+//  Created by chuiseo-MN on 2021/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
