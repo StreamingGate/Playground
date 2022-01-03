6e8412df1438ba31742d9d03c7a9f7302a86f283
chaehui seo <sch0991@naver.com>
2022-01-04 00:23:38 +0900

    design : add landscape ui in play page

--- src/ios/SG_YouTube/SG_YouTube/Play/PlayViewController.swift
+++ src/ios/SG_YouTube/SG_YouTube/Play/PlayViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class PlayViewController: UIViewController {
     @IBOutlet weak var playView: UIView!
+    @IBOutlet weak var playControllView: UIView!
+    var playControllTimer = Timer()
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var viewLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var channelProfileImageView: UIImageView!
     @IBOutlet weak var channelNicknameLabel: UILabel!
+    @IBOutlet weak var channelView: UIView!
     @IBOutlet weak var separatorView: UIView!
+    @IBOutlet weak var stackView: UIStackView!
     
     @IBOutlet weak var chatSendButton: UIButton!
     @IBOutlet weak var chatTextView: UITextView!
@@ -23,14 +27,24 @@ class PlayViewController: UIViewController {
     @IBOutlet weak var chatProfileImageView: UIImageView!
     @IBOutlet weak var chatSeparatorView: UIView!
     @IBOutlet weak var chatViewBottom: NSLayoutConstraint!
+    @IBOutlet weak var playViewTop: NSLayoutConstraint!
+    @IBOutlet weak var playViewCenterX: NSLayoutConstraint!
     
     @IBOutlet weak var explainContainerView: UIView!
-    @IBOutlet weak var chatContainerViewTopMargin: NSLayoutConstraint!
+    @IBOutlet weak var chatContainerViewTop: NSLayoutConstraint!
     @IBOutlet weak var chatContainerView: UIView!
+    @IBOutlet weak var chatContainerViewLeading: NSLayoutConstraint!
+    @IBOutlet weak var chatContainerViewCenterX: NSLayoutConstraint!
+    
+    var portraitLayout: [NSLayoutConstraint] = []
+    var landscapeLayout: [NSLayoutConstraint] = []
+    
+    var safeTop: CGFloat = 0
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        portraitLayout = [chatContainerViewLeading, chatContainerViewCenterX, chatContainerViewTop, playViewTop, playViewCenterX]
         NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(adjustInputView), name: UIResponder.keyboardWillHideNotification, object: nil)
         setUI()
@@ -74,8 +88,59 @@ class PlayViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func enlargeButtonDidTap(_ sender: Any) {
+        if UIDevice.current.value(forKey: "orientation") as? Int == UIInterfaceOrientation.landscapeRight.rawValue {
+            let value = UIInterfaceOrientation.portrait.rawValue
+            UIDevice.current.setValue(value, forKey: "orientation")
+            titleLabel.isHidden = false
+            viewLabel.isHidden = false
+            categoryLabel.isHidden = false
+            stackView.isHidden = false
+            self.view.backgroundColor = UIColor.white
+            NSLayoutConstraint.deactivate(landscapeLayout)
+            NSLayoutConstraint.activate(portraitLayout)
+        } else {
+            let value = UIInterfaceOrientation.landscapeRight.rawValue
+            UIDevice.current.setValue(value, forKey: "orientation")
+            chatContainerView.translatesAutoresizingMaskIntoConstraints = false
+            playView.translatesAutoresizingMaskIntoConstraints = false
+            titleLabel.isHidden = true
+            viewLabel.isHidden = true
+            categoryLabel.isHidden = true
+            stackView.isHidden = true
+            NSLayoutConstraint.deactivate(portraitLayout)
+            landscapeLayout = [chatContainerView.topAnchor.constraint(equalTo: self.view.topAnchor),
+               playView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+               playView.trailingAnchor.constraint(equalTo: self.chatContainerView.leadingAnchor),
+               chatContainerView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width > UIScreen.main.bounds.height ? UIScreen.main.bounds.height : UIScreen.main.bounds.width)]
+            self.view.backgroundColor = UIColor.black
+            NSLayoutConstraint.activate(landscapeLayout)
+        }
+    }
+    
     @IBAction func backTapped(_ sender: Any) {
         chatTextView.resignFirstResponder()
+    }
+    
+    @IBAction func playViewTapped(_ sender: Any) {
+        chatTextView.resignFirstResponder()
+        self.playControllTimer.invalidate()
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+            self.playControllView.alpha = 1
+        } completion: { _ in
+            self.playControllTimer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: false, block: { timer in
+                UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+                    self.playControllView.alpha = 0
+                }
+            })
+        }
+    }
+    
+    @IBAction func playControllerViewTapped(_ sender: Any) {
+        self.playControllTimer.invalidate()
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
+            self.playControllView.alpha = 0
+        }
     }
 }
 
