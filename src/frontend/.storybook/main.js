ce6b17fd12646fb5ae4aeacfc527a0bc564edc23
이재윤/SGS DevCamp <ag502@naver.com>
2022-01-03 22:24:30 +0900

    remove : git cache

--- src/frontend/.storybook/main.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-};
--- src/frontend/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  semi: true,
-  tabWidth: 2,
-  endOfLine: 'lf',
-  singleQuote: true,
-  jsxSingleQuote: true,
-  arrowParens: 'avoid',
-  printWidth: 100,
-};
