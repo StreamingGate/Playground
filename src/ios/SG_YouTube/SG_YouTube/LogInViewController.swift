9638f8ae43749981de1a13a3b10c428278174b03
chaehui seo <sch0991@naver.com>
2022-01-04 17:50:17 +0900

    design : add login page ui

---
+++ src/ios/SG_YouTube/SG_YouTube/LogInViewController.swift
@@ -0,0 +1,55 @@
+//
+//  LogInViewController.swift
+//  SG_YouTube
+//
+//  Created by chuiseo-MN on 2022/01/04.
+//
+
+import Foundation
+import UIKit
+
+class LogInViewController: UIViewController {
+    @IBOutlet weak var idField: UITextField!
+    @IBOutlet weak var pwField: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var forgotPwButton: UIButton!
+    @IBOutlet weak var autoLogInImageView: UIImageView!
+    @IBOutlet weak var autoLogInLabel: UILabel!
+    @IBOutlet weak var autoLogInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    func setUI() {
+        forgotPwButton.titleLabel?.font = UIFont.caption
+        forgotPwButton.setTitleColor(UIColor.placeHolder, for: .normal)
+        autoLogInLabel.font = UIFont.caption
+        autoLogInLabel.textColor = UIColor.placeHolder
+        idField.font = UIFont.Content
+        pwField.font = UIFont.Content
+        logInButton.layer.cornerRadius = 5
+    }
+    
+    @IBAction func logInButtonDidTap(_ sender: Any) {
+        idField.resignFirstResponder()
+        pwField.resignFirstResponder()
+        guard let mainTab = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController else { return }
+        mainTab.modalPresentationStyle = .fullScreen
+        self.present(mainTab, animated: true, completion: nil)
+    }
+    
+    @IBAction func registerButtonDidTap(_ sender: Any) {
+        idField.resignFirstResponder()
+        pwField.resignFirstResponder()
+        guard let registerVC = UIStoryboard(name: "Register", bundle: nil).instantiateViewController(withIdentifier: "RegisterNavigationController") as? RegisterNavigationController else { return }
+        registerVC.modalPresentationStyle = .fullScreen
+        self.present(registerVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func backTapped(_ sender: Any) {
+        idField.resignFirstResponder()
+        pwField.resignFirstResponder()
+    }
+}
