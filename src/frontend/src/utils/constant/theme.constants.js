8b8073cb0ff308ba1178cd02a69be1e1bdf86930
이재윤/SGS DevCamp <ag502@naver.com>
2022-01-04 17:30:05 +0900

    feat : add theme provider

---
+++ src/frontend/src/utils/constant/theme.constants.js
@@ -0,0 +1,27 @@
+const fontSizes = {
+  title: '1.313rem',
+  subtitle: '1.125rem',
+  tab: '1rem',
+  lightTitle: '1,125rem',
+  component: '1rem',
+  content: '0.875rem',
+  hightlightCaption: '0.75rem',
+  caption: '0.75rem',
+  bottomTab: '0.563rem',
+};
+
+const fontWeights = {
+  title: 'bold',
+  subtitle: 'bold',
+  tab: 'bold',
+  lightTitle: 'regular',
+  component: 'regular',
+  content: 'regular',
+  highlightCaption: 'regular',
+  caption: 'regular',
+  bottomTab: 'regular',
+};
+
+const theme = { fontSizes, fontWeights };
+
+export default theme;
