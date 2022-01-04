ed32f08d34a2dbcaf8e4773ab357ad7a933b50b5
ag502 <ag502@naver.com>
2022-01-04 18:04:19 +0900

    fix : fix rem unit and

--- src/frontend/src/utils/constant/theme.constants.js
+++ src/frontend/src/utils/constant/theme.constants.js
@@ -2,10 +2,10 @@ const fontSizes = {
   title: '1.313rem',
   subtitle: '1.125rem',
   tab: '1rem',
-  lightTitle: '1,125rem',
+  lightTitle: '1.125rem',
   component: '1rem',
   content: '0.875rem',
-  hightlightCaption: '0.75rem',
+  highlightCaption: '0.75rem',
   caption: '0.75rem',
   bottomTab: '0.563rem',
 };
@@ -14,12 +14,12 @@ const fontWeights = {
   title: 'bold',
   subtitle: 'bold',
   tab: 'bold',
-  lightTitle: 'regular',
-  component: 'regular',
-  content: 'regular',
-  highlightCaption: 'regular',
-  caption: 'regular',
-  bottomTab: 'regular',
+  lightTitle: 'normal',
+  component: 'normal',
+  content: 'normal',
+  highlightCaption: 'bold',
+  caption: 'normal',
+  bottomTab: 'normal',
 };
 
 const theme = { fontSizes, fontWeights };
