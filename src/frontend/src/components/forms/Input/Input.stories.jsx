b9372af8fc032629e51f8ef698083bbb00b54f55
ag502 <ag502@naver.com>
2022-01-09 22:14:55 +0900

    design : add Input component and storybook

---
+++ src/frontend/src/components/forms/Input/Input.stories.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import Input from './Input';
+
+export default {
+  title: 'Components/Forms/Input',
+  component: Input,
+  args: {
+    type: 'text',
+    placeholder: '아이디를 입력해 주세요.',
+    fontSize: 'content',
+    FullWidth: false,
+  },
+};
+
+const VariantTemplate = args => (
+  <>
+    <Input variant='outlined' {...args} />
+    <Input variant='standard' {...args} />
+  </>
+);
+export const Variant = VariantTemplate.bind({});
+Variant.args = {
+  size: 'small',
+};
+
+const SizeTemplate = args => (
+  <>
+    <Input size='small' variant='contained' {...args} />
+    <Input size='large' variant='contained' {...args} />
+    <Input size='small' variant='standard' {...args} />
+    <Input size='large' variant='standard' {...args} />
+  </>
+);
+export const Size = SizeTemplate.bind({});
+
+const FullWidthTemplate = args => (
+  <>
+    <Input variant='contained' {...args} />
+    <Input variant='standard' {...args} />
+  </>
+);
+export const FullWidth = FullWidthTemplate.bind({});
+FullWidth.args = {
+  fullWidth: true,
+  size: 'small',
+};
