f8f8f348ac2740c4c804798b548210aba4f5b2bf
ag502 <ag502@naver.com>
2022-01-10 10:29:55 +0900

    feat : add login router

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import { GlobalStyle, NormalizeStyle } from '@components/styles';
+import { theme } from '@utils/constant';
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <NormalizeStyle />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navigate to='/login' />} />
+          <Route path='/login' element={<div>TEST</div>} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
 }
 
 export default App;
