0b77604014b8f485068f288240e5f2ccdb7b30f2
ag502 <ag502@naver.com>
2022-01-11 11:50:27 +0900

    design : add login page storybook

---
+++ src/frontend/src/pages/Login/LoginPage/LoginPage.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import LoginPage from './LoginPage';
+
+export default {
+  title: 'Page/Login',
+  component: LoginPage,
+};
+
+const Template = args => <LoginPage {...args} />;
+
+export const Login = Template.bind({});
