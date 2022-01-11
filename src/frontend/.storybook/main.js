a885ef81475e52073459cde010decd051869dcc8
ag502 <ag502@naver.com>
2022-01-11 11:16:10 +0900

    chore : add svgr module in storybook webpack

--- src/frontend/.storybook/main.js
+++ src/frontend/.storybook/main.js
@@ -4,6 +4,10 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: '@storybook/react',
+  // staticDir: ['../src/assets/**'],
+  core: {
+    builder: 'webpack5',
+  },
   webpackFinal: async (config, { configType }) => {
     config.resolve.alias['@'] = path.resolve(__dirname, '../src');
     config.resolve.alias['@assets'] = path.resolve(__dirname, '../src/assets');
@@ -11,6 +15,16 @@ module.exports = {
     config.resolve.alias['@pages'] = path.resolve(__dirname, '../src/pages');
     config.resolve.alias['@services'] = path.resolve(__dirname, '../src/services');
     config.resolve.alias['@utils'] = path.resolve(__dirname, '../src/utils');
+
+    const webpackRules = config.module.rules;
+    const fileLoader = webpackRules.find(rule => rule.test.test('.svg'));
+    fileLoader.exclude = /\.svg$/i;
+
+    webpackRules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
     return config;
   },
 };
