37f90408408b1c8b73b787bc0b36c4f42ddf7323
ag502 <ag502@naver.com>
2022-01-11 11:40:04 +0900

    refactor : add storybook webpack alias function

--- src/frontend/.storybook/main.js
+++ src/frontend/.storybook/main.js
@@ -1,20 +1,24 @@
 const path = require('path');
 
+function getAlias(config) {
+  const { resolve } = config;
+  resolve.alias['@'] = path.resolve(__dirname, '../src');
+  resolve.alias['@assets'] = path.resolve(__dirname, '../src/assets');
+  resolve.alias['@components'] = path.resolve(__dirname, '../src/components');
+  resolve.alias['@pages'] = path.resolve(__dirname, '../src/pages');
+  resolve.alias['@services'] = path.resolve(__dirname, '../src/services');
+  resolve.alias['@utils'] = path.resolve(__dirname, '../src/utils');
+}
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx)'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: '@storybook/react',
-  // staticDir: ['../src/assets/**'],
   core: {
     builder: 'webpack5',
   },
   webpackFinal: async (config, { configType }) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, '../src');
-    config.resolve.alias['@assets'] = path.resolve(__dirname, '../src/assets');
-    config.resolve.alias['@components'] = path.resolve(__dirname, '../src/components');
-    config.resolve.alias['@pages'] = path.resolve(__dirname, '../src/pages');
-    config.resolve.alias['@services'] = path.resolve(__dirname, '../src/services');
-    config.resolve.alias['@utils'] = path.resolve(__dirname, '../src/utils');
+    getAlias(config);
 
     const webpackRules = config.module.rules;
     const fileLoader = webpackRules.find(rule => rule.test.test('.svg'));
