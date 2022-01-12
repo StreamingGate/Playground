c1a451da1fa7c91949c9e89bbfc4e546225fdcd9
chaehui seo <sch0991@naver.com>
2022-01-12 22:32:59 +0900

    design : add tabBar visibility control extension

---
+++ src/ios/SG_YouTube/SG_YouTube/Extensions/UITabBarController+Visibility.swift
@@ -0,0 +1,58 @@
+//
+//  UITabBarController+Visibility.swift
+//  SG_YouTube
+//
+//  Created by chuiseo-MN on 2022/01/12.
+//
+
+import Foundation
+import UIKit
+
+extension UITabBarController {
+    func setTabBar(
+        hidden: Bool,
+        animated: Bool = true,
+        along transitionCoordinator: UIViewControllerTransitionCoordinator? = nil
+    ) {
+        guard isTabBarHidden != hidden else { return }
+
+        let offsetY = hidden ? tabBar.frame.height : -tabBar.frame.height
+        let endFrame = tabBar.frame.offsetBy(dx: 0, dy: offsetY)
+        let vc: UIViewController? = viewControllers?[selectedIndex]
+        var newInsets: UIEdgeInsets? = vc?.additionalSafeAreaInsets
+        let originalInsets = newInsets
+        newInsets?.bottom -= offsetY
+
+        func set(childViewController cvc: UIViewController?, additionalSafeArea: UIEdgeInsets) {
+            cvc?.additionalSafeAreaInsets = additionalSafeArea
+            cvc?.view.setNeedsLayout()
+        }
+
+        if hidden, let insets = newInsets { set(childViewController: vc, additionalSafeArea: insets) }
+
+        guard animated else {
+            tabBar.frame = endFrame
+            return
+        }
+        
+        weak var tabBarRef = self.tabBar
+        if let tc = transitionCoordinator {
+            tc.animateAlongsideTransition(in: self.view, animation: { _ in tabBarRef?.frame = endFrame }) { context in
+                if !hidden, let insets = context.isCancelled ? originalInsets : newInsets {
+                    set(childViewController: vc, additionalSafeArea: insets)
+                }
+            }
+        } else {
+            UIView.animate(withDuration: 0.3, animations: { tabBarRef?.frame = endFrame }) { didFinish in
+                if !hidden, didFinish, let insets = newInsets {
+                    set(childViewController: vc, additionalSafeArea: insets)
+                }
+            }
+        }
+    }
+
+    var isTabBarHidden: Bool {
+        return !tabBar.frame.intersects(view.frame)
+    }
+
+}
