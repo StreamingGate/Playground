b066bbc8c5fafdd128e148f0fd178309379e41ee
ag502 <ag502@naver.com>
2022-01-12 21:33:30 +0900

    fix : add min-width property in Svg Icon

--- src/frontend/src/components/cores/Icons/withSvgIcon.jsx
+++ src/frontend/src/components/cores/Icons/withSvgIcon.jsx
@@ -8,12 +8,14 @@ const getSvgIconSize = size => {
       return css`
         width: 36px;
         height: 36px;
+        min-width: 36px;
       `;
     // default size is 'small'
     default:
       return css`
         width: 24px;
         height: 24px;
+        min-width: 24px;
       `;
   }
 };
