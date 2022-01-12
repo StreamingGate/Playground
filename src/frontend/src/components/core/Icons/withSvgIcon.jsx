861a27fd30c656611f2aad92e63e85745d68c528
ag502 <ag502@naver.com>
2022-01-12 20:04:07 +0900

    feat : add Svg Icon HOC

---
+++ src/frontend/src/components/core/Icons/withSvgIcon.jsx
@@ -0,0 +1,41 @@
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+const getSvgIconSize = size => {
+  switch (size) {
+    case 'large':
+      return css`
+        width: 36px;
+        height: 36px;
+      `;
+    // default size is 'small'
+    default:
+      return css`
+        width: 24px;
+        height: 24px;
+      `;
+  }
+};
+
+function withSvgIcon(SvgIcon) {
+  function SubComponent({ size, ...rest }) {
+    return <Temp size={size} {...rest} />;
+  }
+
+  const Temp = styled(SvgIcon)`
+    ${({ size }) => getSvgIconSize(size)}
+  `;
+
+  SubComponent.propTypes = {
+    size: PropTypes.oneOf(['small', 'large']),
+  };
+
+  SubComponent.defaultProps = {
+    size: 'small',
+  };
+
+  return memo(SubComponent);
+}
+
+export default withSvgIcon;
