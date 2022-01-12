9001d82ed16312afdf06e6c742d5dc390d82f9dc
ag502 <ag502@naver.com>
2022-01-12 20:49:12 +0900

    chore : add Alarm Icon svg

--- src/frontend/src/components/cores/index.js
+++ src/frontend/src/components/cores/index.js
@@ -3,3 +3,4 @@ export { default as Typography } from './Typography/Typography';
 export { default as VerticalLogo } from '@assets/icons/VerticalLogo.svg';
 export { default as AddEmptyCircle } from './Icons/AddEmptyCircle';
 export { default as HamburgerBar } from './Icons/HamburgerBar';
+export { default as Alarm } from './Icons/Alarm';
