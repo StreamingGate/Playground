7752a8081ba45eb9b9b0d0501daa8b0b8a410073
ag502 <ag502@naver.com>
2022-01-12 20:44:01 +0900

    chore : add Hamburger Icon svg

--- src/frontend/src/components/cores/index.js
+++ src/frontend/src/components/cores/index.js
@@ -2,3 +2,4 @@ export { default as Typography } from './Typography/Typography';
 
 export { default as VerticalLogo } from '@assets/icons/VerticalLogo.svg';
 export { default as AddEmptyCircle } from './Icons/AddEmptyCircle';
+export { default as HamburgerBar } from './Icons/HamburgerBar';
