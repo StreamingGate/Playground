bf9804034536e7aa39f34070b3f8d0426356aecf
ag502 <ag502@naver.com>
2022-01-13 14:03:54 +0900

    design : add temp Profile component

---
+++ src/frontend/src/components/layouts/Header/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { IconButton } from '@components/buttons';
+import { Alarm } from '@components/cores';
+
+function Profile() {
+  return (
+    <IconButton>
+      <Alarm />
+    </IconButton>
+  );
+}
+
+export default Profile;
