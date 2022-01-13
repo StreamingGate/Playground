2c0659586ce292958f9b7389013a3e52dbd425b4
ag502 <ag502@naver.com>
2022-01-13 14:01:50 +0900

    feat : add onClick property

--- src/frontend/src/components/buttons/IconButton/IconButton.jsx
+++ src/frontend/src/components/buttons/IconButton/IconButton.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 import S from './IconButton.style';
 
-function IconButton({ className, size, children }) {
+function IconButton({ className, size, children, onClick }) {
   return (
-    <S.Button className={className} type='button' size={size}>
+    <S.Button className={className} type='button' size={size} onClick={onClick}>
       {children}
     </S.Button>
   );
@@ -15,11 +15,13 @@ IconButton.propTypes = {
   className: PropTypes.string,
   size: PropTypes.oneOf(['small', 'large']),
   children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
 };
 
 IconButton.defaultProps = {
   className: '',
   size: 'small',
+  onClick: undefined,
 };
 
 export default IconButton;
