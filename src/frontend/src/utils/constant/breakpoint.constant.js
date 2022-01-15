8bbb21070efccca6148c008e82be71c5f9d5cfed
ag502 <ag502@naver.com>
2022-01-16 03:24:49 +0900

    fix : modify wide laptop query

--- src/frontend/src/utils/constant/breakpoint.constant.js
+++ src/frontend/src/utils/constant/breakpoint.constant.js
@@ -7,7 +7,7 @@ const screenSize = {
 };
 
 const queries = {
-  wideLaptop: `max-width ${screenSize.wideLaptop / 16}rem`,
+  wideLaptop: `max-width: ${screenSize.wideLaptop / 16}rem`,
   laptopMax: `max-width: ${screenSize.laptop / 16}rem`,
   tabletMax: `max-width: ${screenSize.tablet / 16}rem`,
   mobileMax: `max-width: ${screenSize.mobile / 16}rem`,
