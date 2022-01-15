23241e67d47c10f5ffbb2965e3a3e883444e5e96
ag502 <ag502@naver.com>
2022-01-15 13:43:59 +0900

    chore : add wide laptop screen size

--- src/frontend/src/utils/constant/breakpoint.constant.js
+++ src/frontend/src/utils/constant/breakpoint.constant.js
@@ -1,4 +1,5 @@
 const screenSize = {
+  wideLaptop: 1200,
   laptop: 1023.99,
   tablet: 767.99,
   mobile: 450,
@@ -6,6 +7,7 @@ const screenSize = {
 };
 
 const queries = {
+  wideLaptop: `max-width ${screenSize.wideLaptop / 16}rem`,
   laptopMax: `max-width: ${screenSize.laptop / 16}rem`,
   tabletMax: `max-width: ${screenSize.tablet / 16}rem`,
   mobileMax: `max-width: ${screenSize.mobile / 16}rem`,
