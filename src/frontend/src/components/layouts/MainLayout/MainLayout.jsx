faf09981bb956b76da275db24476ebca2830017c
ag502 <ag502@naver.com>
2022-01-16 01:55:27 +0900

    feat : add BackDrop appearance logic

--- src/frontend/src/components/layouts/MainLayout/MainLayout.jsx
+++ src/frontend/src/components/layouts/MainLayout/MainLayout.jsx
@@ -3,40 +3,59 @@ import React, { useEffect, useState, useMemo } from 'react';
 import { MainLayoutContext } from '@utils/context';
 import { useWindowSize } from '@utils/hook';
 import { breakPoint } from '@utils/constant';
+import S from './MainLayout.style';
 
-import { Header, SideNavigation } from '../index';
+import Header from '../Header/Header';
+import SideNavigation from '../SideNavigation/SideNavigation';
+import SideFriendList from '../SideFriendList/SideFriendList';
 
-function setInitNavState(innerWidth) {
-  const { screenSize } = breakPoint;
-  return innerWidth > screenSize.wideLaptop;
-}
+const { screenSize } = breakPoint;
 
 function MainLayout() {
   const { innerWidth } = useWindowSize();
-  const [isSideNavOpen, setSideNavOpen] = useState(true);
+  const [sideNavState, setSideNavState] = useState({ open: false, backdrop: false });
+  const [sideFriendState, setSideFriendState] = useState({ open: false, backdrop: false });
+
+  const setInitNavState = () => {
+    // 비디오 재생 화면, wide laptop 사이즈 보다 작을때 backdrop true!!
+    if (innerWidth > screenSize.wideLaptop) {
+      setSideNavState({ open: true, backdrop: false });
+    } else {
+      setSideNavState({ open: false, backdrop: true });
+    }
+  };
+
+  const setInitFriendListState = () => {
+    // 비디오 재생 화면, laptop 사이즈 보다 작을때 backdrop true!!
+    if (innerWidth > screenSize.laptop) {
+      setSideFriendState({ open: true, backdrop: false });
+    } else {
+      setSideFriendState({ open: false, backdrop: true });
+    }
+  };
 
   useEffect(() => {
-    const isOpen = setInitNavState(innerWidth);
-    if (isOpen) {
-      setSideNavOpen(true);
-    } else {
-      setSideNavOpen(false);
-    }
+    setInitNavState();
+    setInitFriendListState();
   }, [innerWidth]);
 
   const handleToggleSideNav = () => {
-    setSideNavOpen(prev => !prev);
+    setSideNavState(prev => ({ ...prev, open: !prev.open }));
   };
 
   const mainLayoutContextValue = useMemo(
-    () => ({ isSideNavOpen, onToggleSideNav: handleToggleSideNav }),
-    [isSideNavOpen]
+    () => ({ sideNavState, sideFriendState, onToggleSideNav: handleToggleSideNav }),
+    [sideNavState]
   );
 
   return (
     <MainLayoutContext.Provider value={mainLayoutContextValue}>
       <Header />
-      <SideNavigation />
+      <S.Temp>
+        <SideNavigation />
+        <S.MainContentContainer>Content</S.MainContentContainer>
+        <SideFriendList />
+      </S.Temp>
     </MainLayoutContext.Provider>
   );
 }
