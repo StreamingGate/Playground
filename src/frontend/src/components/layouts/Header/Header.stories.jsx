b08a58b054a3692106f22c123c39137d1530677a
ag502 <ag502@naver.com>
2022-01-16 12:38:42 +0900

    fix : resolve context error in Header

--- src/frontend/src/components/layouts/Header/Header.stories.jsx
+++ src/frontend/src/components/layouts/Header/Header.stories.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 
+import { MainLayoutContext } from '@utils/context';
+
 import Header from './Header';
 
 export default {
   title: 'Layouts/Header',
   component: Header,
+  decorators: [
+    (Story, rest) => {
+      const { args } = rest;
+      return (
+        <MainLayoutContext.Provider value={{ ...args.ctx }}>
+          <div style={{ flex: '1' }}>
+            <Story />
+          </div>
+        </MainLayoutContext.Provider>
+      );
+    },
+  ],
   parameters: {
     layout: 'fullscreen',
+    controls: { hideNoControlsWarning: true, exclude: ['ctx'] },
+    previewTabs: {
+      'storybook/docs/panel': { hidden: true },
+    },
   },
-  decorators: [
-    Story => (
-      <div style={{ flex: '1' }}>
-        <Story />
-      </div>
-    ),
-  ],
 };
 
 const Template = args => <Header {...args} />;
+
 export const HeaderStory = Template.bind({});
+HeaderStory.args = {
+  ctx: { onToggleSideNav: undefined },
+};
 HeaderStory.storyName = 'Header';
