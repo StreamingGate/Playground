10758b4423589ea74a2814c238a876951572e6f7
woojae <37957608+kmswlee@users.noreply.github.com>
2022-01-17 01:41:43 +0900

    refactor : rename package

--- src/server/user-service/src/main/java/com/example/userservice/dto/UserDto.java
+++ src/server/user-service/src/main/java/com/example/userservice/dto/UserDto.java
@@ -1,9 +1,10 @@
 package com.example.userservice.dto;
 
 import lombok.Data;
+
 import java.time.LocalDate;
 
-//@Data
+@Data
 public class UserDto {
     private String email;
     private String uuid;
