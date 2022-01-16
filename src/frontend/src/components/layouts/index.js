4ecb1728d2d8a9be14f51bb4998568119dd089e8
ag502 <ag502@naver.com>
2022-01-16 12:53:23 +0900

    chore : LoginPage hide no control error

--- src/frontend/src/components/layouts/index.js
+++ src/frontend/src/components/layouts/index.js
@@ -1,3 +1,1 @@
-export { default as Header } from './Header/Header';
-export { default as SideNavigation } from './SideNavigation/SideNavigation';
 export { default as MainLayout } from './MainLayout/MainLayout';
