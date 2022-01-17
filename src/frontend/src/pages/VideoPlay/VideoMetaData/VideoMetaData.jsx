11145bf5f0f8c66afe6c0d74590c6aedda6fb5f4
ag502 <ag502@naver.com>
2022-01-17 23:23:05 +0900

    design : add VideoMetaData Component

--- src/frontend/src/pages/VideoPlay/VideoMetaData/VideoMetaData.jsx
+++ src/frontend/src/pages/VideoPlay/VideoMetaData/VideoMetaData.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as S from './VideoMetaData.style';
 
 import { IconButton } from '@components/buttons';
 import { ThumbUp, ThumbDown, Share, Report } from '@components/cores';
+
+function ActionButton({ element, content, onClick }) {
+  return (
+    <S.Action>
+      <IconButton onClick={onClick}>{element}</IconButton>
+      <S.ActionLabel type='caption'>{content}</S.ActionLabel>
+    </S.Action>
+  );
+}
 
 function VideoMetaData() {
   return (
@@ -13,34 +23,36 @@ function VideoMetaData() {
       <S.VideoInfoContainer>
         <S.WatchPeople type='caption'>6702명 시청 중</S.WatchPeople>
         <S.ActionContainer>
-          <S.Action>
-            <IconButton>
-              <ThumbUp />
-            </IconButton>
-            <S.ActionLabel type='caption'>8.5회</S.ActionLabel>
-          </S.Action>
-          <S.Action>
-            <IconButton>
-              <ThumbDown />
-            </IconButton>
-            <S.ActionLabel type='caption'>싫어요</S.ActionLabel>
-          </S.Action>
-          <S.Action>
-            <IconButton>
-              <Share />
-            </IconButton>
-            <S.ActionLabel type='caption'>공유</S.ActionLabel>
-          </S.Action>
-          <S.Action>
-            <IconButton>
-              <Report />
-            </IconButton>
-            <S.ActionLabel type='caption'>신고</S.ActionLabel>
-          </S.Action>
+          <ActionButton element={<ThumbUp />} content='8.5명' />
+          <ActionButton element={<ThumbDown />} content='싫어요' />
+          <ActionButton element={<Share />} content='공유' />
+          <ActionButton element={<Report />} content='신고' />
         </S.ActionContainer>
       </S.VideoInfoContainer>
+      <S.VideoSubInfoContainer>
+        <S.MyProfile size='large' />
+        <S.VideoSubInfo>
+          <S.ChannelInfo>
+            <S.ChannelName>채널 이름</S.ChannelName>
+            <S.SubscribePeople type='bottomTab'>2.01천 명</S.SubscribePeople>
+          </S.ChannelInfo>
+          <S.ContentOverview>
+            한 달 후, 천 년 만에 찾아온다는 혜성을 기다리고 있는 일본. 산골 깊은 시골 마을에 살고
+            있는 여고생 미츠하는 우울한 나날을 보내고 있다. 촌장인 아버지의 선거활동과 신사 집안의
+            낡은 풍습. 좁고 작은 마을에서는 주위의 시선이 너무나도 신경 쓰이는 나이인 만큼 도시를
+            향한 동경심은 커지기만 한다.
+          </S.ContentOverview>
+        </S.VideoSubInfo>
+        <S.SubScribeBtn>친구 신청</S.SubScribeBtn>
+      </S.VideoSubInfoContainer>
     </S.VideoMetaDataContainer>
   );
 }
+
+ActionButton.propTypes = {
+  element: PropTypes.element.isRequired,
+  content: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default VideoMetaData;
--- src/frontend/src/pages/VideoPlay/VideoMetaData/VideoMetaData.style.js
+++ src/frontend/src/pages/VideoPlay/VideoMetaData/VideoMetaData.style.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 import { Typography } from '@components/cores';
+import { Avatar } from '@components/dataDisplays';
+import { Button } from '@components/buttons';
 
 export const VideoMetaDataContainer = styled.div``;
 
@@ -15,7 +17,7 @@ export const VideoTitle = styled(Typography)`
 export const VideoInfoContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
   padding-bottom: 10px;
   border-bottom: 1px solid ${({ theme }) => theme.colors.separator};
 `;
@@ -32,7 +34,49 @@ export const ActionContainer = styled.div`
 export const Action = styled.div`
   display: flex;
   align-items: center;
-  gap: 9px;
 `;
 
 export const ActionLabel = styled(Typography)``;
+
+export const VideoSubInfoContainer = styled.div`
+  display: flex;
+  margin-top: 15px;
+`;
+
+export const MyProfile = styled(Avatar)`
+  flex-shrink: 0;
+  margin-right: 5px;
+`;
+
+export const VideoSubInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 17px;
+  flex-grow: 1;
+`;
+
+export const ChannelInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ChannelName = styled(Typography)``;
+
+export const SubscribePeople = styled(Typography)`
+  color: ${({ theme }) => theme.colors.customDarkGray};
+`;
+
+export const ContentOverview = styled(Typography)`
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+  text-overflow: ellopsis;
+  width: 60%;
+`;
+
+export const SubScribeBtn = styled(Button)`
+  flex-shrink: 0;
+  padding: 5px 10px;
+  height: fit-content;
+`;
