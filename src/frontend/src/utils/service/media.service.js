7264621fdf946ff367ff3a58f8825338633d8fb2
ag502 <ag502@naver.com>
2022-01-19 21:11:32 +0900

    feat : add initial profile generator

--- src/frontend/src/utils/service/media.service.js
+++ src/frontend/src/utils/service/media.service.js
@@ -12,4 +12,28 @@ const getImagePreviewURl = image => {
   });
 };
 
-export default { getImagePreviewURl };
+const createImageFromInitials = (name, bgColor, color) => {
+  const size = 100;
+  const firstLetter = name[0];
+
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = size;
+  canvas.height = size;
+
+  context.fillStyle = bgColor;
+  context.fillRect(0, 0, size, size);
+
+  context.fillStyle = `${color}50`;
+  context.fillRect(0, 0, size, size);
+
+  context.fillStyle = color;
+  context.textBaseline = 'middle';
+  context.textAlign = 'center';
+  context.font = `${size / 2}px Roboto`;
+  context.fillText(firstLetter, size / 2, size / 2);
+
+  return canvas.toDataURL();
+};
+
+export default { getImagePreviewURl, createImageFromInitials };
