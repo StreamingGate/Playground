56230341fc06333736bdf3eb82ad995880481a0a
kimhanui <cocacola2017@naver.com>
2022-01-21 00:11:37 +0900

    feat : add relation(User-LiveRoom), remove State

--- data/mariadb/schema.sql
+++ data/mariadb/schema.sql
@@ -4,43 +4,41 @@ create table friend
     fr_id            bigint,
     fr_nickname      varchar(255),
     fr_profile_image varchar(255),
-    users_id         bigint,
+    user_id          bigint,
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 create table friend_wait
 (
     id                bigint not null auto_increment,
-    receiver_nickname varchar(8),
-    sender_nickname   varchar(8),
+    nickname_receiver varchar(8),
+    nickname_sender   varchar(8),
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 create table live_room
 (
-    id              bigint   not null auto_increment,
-    category        varchar(10),
-    chat_room_id    varchar(255),
-    created_at      datetime,
-    content         varchar(5000),
-    dislike_cnt     integer  not null,
-    host_email      varchar(30),
-    like_cnt        integer  not null,
-    live_room_state varchar(10),
-    report_cnt      smallint not null,
-    streaming_id    varchar(255),
-    title           varchar(100),
+    id           bigint   not null auto_increment,
+    category     varchar(10),
+    chat_room_id varchar(255),
+    content      varchar(5000),
+    created_at   datetime,
+    host_email   varchar(30),
+    like_cnt     integer  not null,
+    report_cnt   smallint not null,
+    streaming_id varchar(255),
+    title        varchar(100),
+    users_id     bigint,
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 create table live_viewer
 (
     id           bigint not null auto_increment,
     disliked     bit    not null,
     liked        bit    not null,
-    role         varchar(255),
-    state        integer,
+    role         varchar(10),
     live_room_id bigint,
-    users_id     bigint,
+    user_id      bigint,
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 create table metadata
 (
     id               bigint not null auto_increment,
@@ -51,7 +49,7 @@ create table metadata
     uploader_email   varchar(30),
     video_created_at datetime,
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 create table users
 (
     id            bigint                         not null auto_increment,
@@ -68,7 +66,7 @@ create table users
     time_zone     varchar(255),
     uuid          varchar(36),
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 create table video
 (
     id          bigint   not null auto_increment,
@@ -82,7 +80,7 @@ create table video
     metadata_id bigint,
     users_id    bigint,
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 create table viewed_history
 (
     id              bigint not null auto_increment,
@@ -94,20 +92,22 @@ create table viewed_history
     users_id        bigint,
     video_id        bigint,
     primary key (id)
-) engine = InnoDB;
+) engine=InnoDB
 alter table users
-    add constraint UK_6dotkott2kjsp8vw4d0m25fb7 unique (email);
+    add constraint UK_6dotkott2kjsp8vw4d0m25fb7 unique (email)
 alter table friend
-    add constraint FK77hwidnt50k76utjsr02f15dx foreign key (users_id) references users (id);
+    add constraint FKeab81424e9dtc4a8hjlq4xiew foreign key (user_id) references users (id)
+alter table live_room
+    add constraint FK6vnxsrdcyyl32b00uvr4mdt2e foreign key (users_id) references users (id)
 alter table live_viewer
-    add constraint FK6swae88o8jsovsq1hbk50kvg6 foreign key (live_room_id) references live_room (id);
+    add constraint FK6swae88o8jsovsq1hbk50kvg6 foreign key (live_room_id) references live_room (id)
 alter table live_viewer
-    add constraint FKl4jneonk8e7lghuvykdkck79l foreign key (users_id) references users (id);
+    add constraint FKovjr1pogd7rjlfah9i13dm2o5 foreign key (user_id) references users (id)
 alter table video
-    add constraint FKlq8ktyke2jim62cq0gnu2ys3s foreign key (metadata_id) references metadata (id);
+    add constraint FKlq8ktyke2jim62cq0gnu2ys3s foreign key (metadata_id) references metadata (id)
 alter table video
-    add constraint FKbumohg6sht50cdc9spya45hp5 foreign key (users_id) references users (id);
+    add constraint FKbumohg6sht50cdc9spya45hp5 foreign key (users_id) references users (id)
 alter table viewed_history
-    add constraint FKhs9qlglx4cco197tva5upwvc0 foreign key (users_id) references users (id);
+    add constraint FKhs9qlglx4cco197tva5upwvc0 foreign key (users_id) references users (id)
 alter table viewed_history
-    add constraint FKjr82k6pv411gu2ji3865w6kqr foreign key (video_id) references video (id);
+    add constraint FKjr82k6pv411gu2ji3865w6kqr foreign key (video_id) references video (id)
