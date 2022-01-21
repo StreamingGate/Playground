847fa0a8b878972e16d643aa5dc1da6d6186d547
ag502 <ag502@naver.com>
2022-01-22 02:21:57 +0900

    feat : add input validation and toched logic

--- src/frontend/src/utils/hook/useForm.js
+++ src/frontend/src/utils/hook/useForm.js
@@ -1,7 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ValidationError } from 'yup';
 
-export default function useForm(initialValues) {
+export default function useForm({ initialValues, validSchema }) {
   const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState(
+    Object.keys(initialValues).reduce((acc, name) => {
+      return { ...acc, [name]: false };
+    }, {})
+  );
+
+  const checkError = async () => {
+    try {
+      await validSchema.validate(values, { abortEarly: false });
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        const validationErrors = {};
+        error.inner.forEach(err => {
+          validationErrors[err.path] = err.message;
+        });
+
+        setErrors(validationErrors);
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkError();
+  }, [values]);
 
   const handleInputChange = e => {
     const { name, value } = e.target;
@@ -10,5 +36,14 @@ export default function useForm(initialValues) {
     setValues(newValues);
   };
 
-  return { values, handleInputChange };
+  const handleInputBlur = e => {
+    const { name } = e.target;
+
+    setTouched(prev => ({
+      ...prev,
+      [name]: true,
+    }));
+  };
+
+  return { values, errors, touched, handleInputChange, handleInputBlur };
 }
