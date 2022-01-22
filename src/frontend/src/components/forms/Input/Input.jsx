5530633d5a7b5fe04128b44d725acbd77631a2e1
ag502 <ag502@naver.com>
2022-01-22 21:14:06 +0900

    design : add helper text in Input Component

--- src/frontend/src/components/forms/Input/Input.jsx
+++ src/frontend/src/components/forms/Input/Input.jsx
@@ -1,12 +1,14 @@
 import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 
-import InputStyle from './Input.styles';
+import * as S from './Input.styles';
 
 const Input = forwardRef((props, ref) => {
   const {
     className,
     type,
+    error,
+    helperText,
     placeholder,
     value,
     size,
@@ -17,25 +19,30 @@ const Input = forwardRef((props, ref) => {
     ...rest
   } = props;
   return (
-    <InputStyle.Input
-      className={className}
-      ref={ref}
-      type={type}
-      placeholder={placeholder}
-      value={value}
-      size={size}
-      variant={variant}
-      fullWidth={fullWidth}
-      fontSize={fontSize}
-      onChange={onChange}
-      {...rest}
-    />
+    <>
+      <S.Input
+        className={className}
+        ref={ref}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        size={size}
+        variant={variant}
+        fullWidth={fullWidth}
+        fontSize={fontSize}
+        onChange={onChange}
+        {...rest}
+      />
+      {error && <S.HelperText type='caption'>{helperText}</S.HelperText>}
+    </>
   );
 });
 
 Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.oneOf(['text', 'password', 'file']),
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
   placeholder: PropTypes.string,
   value: PropTypes.string,
   size: PropTypes.oneOf(['sm', 'md', 'lg']),
@@ -58,6 +65,8 @@ Input.propTypes = {
 Input.defaultProps = {
   className: '',
   type: 'text',
+  error: false,
+  helperText: '',
   placeholder: '',
   value: undefined,
   size: 'md',
