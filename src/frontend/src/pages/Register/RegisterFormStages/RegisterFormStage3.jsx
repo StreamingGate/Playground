53141bb644f15cfcaafcaafc33e9eb69f982f469
ag502 <ag502@naver.com>
2022-01-22 14:24:15 +0900

    feat : apply useForm in FormStage3

--- src/frontend/src/pages/Register/RegisterFormStages/RegisterFormStage3.jsx
+++ src/frontend/src/pages/Register/RegisterFormStages/RegisterFormStage3.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 
 import * as S from './RegisterFormStages.style';
 
-function RegisterFormState3() {
+function RegisterFormState3({ values, errors, touched, onChange, onBlur }) {
+  const { password, passwordCheck } = values;
+
   return (
     <>
       <S.FormStageInputContainer>
         <S.InputLabel>비밀번호</S.InputLabel>
-        <S.StageInput type='password' size='sm' fullWidth />
+        <S.StageInput
+          name='password'
+          type='password'
+          size='sm'
+          fullWidth
+          value={password}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        {touched.password && errors.passwords && <div>{errors.passwords}</div>}
       </S.FormStageInputContainer>
       <S.FormStageInputContainer>
         <S.InputLabel>비밀번호 확인</S.InputLabel>
-        <S.StageInput type='password' size='sm' fullWidth />
+        <S.StageInput
+          name='passwordCheck'
+          type='password'
+          size='sm'
+          fullWidth
+          value={passwordCheck}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        {touched.passwordCheck && errors.passwordsCheck && <div>{errors.passwordsCheck}</div>}
       </S.FormStageInputContainer>
     </>
   );
