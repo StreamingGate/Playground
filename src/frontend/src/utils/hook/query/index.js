3b1ab6bc1b246f5d691d5d0a6688185ca8237683
ag502 <ag502@naver.com>
2022-01-23 23:17:21 +0900

    refactor : modify register react query

--- src/frontend/src/utils/hook/query/index.js
+++ src/frontend/src/utils/hook/query/index.js
@@ -1,4 +1,2 @@
 export { default as useVerifyEmaii } from './useVerifyEmail';
-export { default as useVerifyCode } from './useVerifyCode';
-export { default as useVerifyNickName } from './useVerifyNickName';
 export { default as useUserRegister } from './useUserRegister';
--- src/frontend/src/utils/hook/query/useUserRegister.js
+++ src/frontend/src/utils/hook/query/useUserRegister.js
@@ -5,10 +5,37 @@ import axios from 'axios';
 const postUserRegister = async values => {
   console.log(values);
   const { data } = await axios.post(`${process.env.REACT_APP_API}/users`, { values });
+  return data;
 };
 
-export default function useUserRegister(onSuccess) {
-  return useMutation(values => postUserRegister(values), {
+// /users/mail?code=#
+const postVerifyCode = async code => {
+  console.log(code);
+  const { data } = await axios.post(`${process.env.REACT_APP_API}/posts`);
+  return data;
+};
+
+// /nickname?nickname={nickname}
+const postVerifyNickName = async nickName => {
+  console.log(nickName);
+  const { data } = await axios.post(`${process.env.REACT_APP_API}/posts`);
+  return data;
+};
+
+export default function useUserRegister(type, onSuccess) {
+  let requestFn = null;
+  switch (type) {
+    case 'verify-code':
+      requestFn = postVerifyCode;
+      break;
+    case 'verify-nickname':
+      requestFn = postVerifyNickName;
+      break;
+    default:
+      requestFn = postUserRegister;
+  }
+
+  return useMutation(arg => requestFn(arg), {
     enabled: false,
     onSuccess,
   });
