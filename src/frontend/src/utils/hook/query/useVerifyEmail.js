f12e4772fd837e7bb9e63cf7b495abb707bc89f1
ag502 <ag502@naver.com>
2022-01-23 18:55:58 +0900

    feat : add send verify email query hook

---
+++ src/frontend/src/utils/hook/query/useVerifyEmail.js
@@ -0,0 +1,14 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const postVerifyEmail = async email => {
+  const { data } = await axios.post(`${process.env.REACT_APP_API}/posts`);
+  return data;
+};
+
+export default function useVerifyEmaii(emailAddress, onSuccess) {
+  return useQuery(['verify-email', emailAddress], () => postVerifyEmail(emailAddress), {
+    enabled: false,
+    onSuccess,
+  });
+}
