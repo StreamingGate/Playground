45e345300c313e330d098892f8aaff918238f20c
ag502 <ag502@naver.com>
2022-01-23 22:54:53 +0900

    fix : useQuery to useMutation

--- src/frontend/src/utils/hook/query/useVerifyCode.js
+++ src/frontend/src/utils/hook/query/useVerifyCode.js
@@ -1,14 +1,15 @@
-import { useQuery } from 'react-query';
+import { useMutation } from 'react-query';
 import axios from 'axios';
 
 // /users/mail?code=#
 const postVerifyCode = async code => {
+  console.log(code);
   const { data } = await axios.post(`${process.env.REACT_APP_API}/posts`);
   return data;
 };
 
-export default function useVerifyCode(code, onSuccess) {
-  return useQuery('verify-code', () => postVerifyCode(code), {
+export default function useVerifyCode(onSuccess) {
+  return useMutation(code => postVerifyCode(code), {
     enabled: false,
     onSuccess,
   });
--- src/frontend/src/utils/hook/query/useVerifyNickName.js
+++ src/frontend/src/utils/hook/query/useVerifyNickName.js
@@ -1,14 +1,15 @@
-import { useQuery } from 'react-query';
+import { useMutation } from 'react-query';
 import axios from 'axios';
 
 // /nickname?nickname={nickname}
 const postVerifyNickName = async nickName => {
+  console.log(nickName);
   const { data } = await axios.post(`${process.env.REACT_APP_API}/posts`);
   return data;
 };
 
-export default function useVerifyNickName(nickName, onSuccess) {
-  return useQuery('verify-nickname', () => postVerifyNickName(nickName), {
+export default function useVerifyNickName(onSuccess) {
+  return useMutation(nickName => postVerifyNickName(nickName), {
     enabled: false,
     onSuccess,
   });
