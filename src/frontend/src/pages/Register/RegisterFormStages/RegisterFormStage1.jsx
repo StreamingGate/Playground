1bf7bfe5cbb5db4b0af98c493fa89edb27f78832
ag502 <ag502@naver.com>
2022-01-23 15:39:55 +0900

    feat : add email verify timer

--- src/frontend/src/pages/Register/RegisterFormStages/RegisterFormStage1.jsx
+++ src/frontend/src/pages/Register/RegisterFormStages/RegisterFormStage1.jsx
@@ -1,14 +1,50 @@
-import React, { useEffect, useState, memo } from 'react';
+import React, { useEffect, useState, memo, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import * as S from './RegisterFormStages.style';
 
 import { Button } from '@components/buttons';
 
+const VERIFY_TIME = 60 * 10;
+
+function parseSecond(seconds) {
+  const min = parseInt(seconds / 60, 10);
+  const sec = seconds % 60;
+  return { min, sec };
+}
+
 function RegisterFormStage1({ values, errors, touched, onChange, onBlur }) {
   const { name, email, verify } = values;
+  const limitTime = useRef(VERIFY_TIME);
+  const timer = useRef(null);
+
+  const [countDown, setCountDown] = useState(parseSecond(limitTime.current));
   const [isVerify, setIsVerify] = useState(false);
   const [isVerifyBtnDisable, setVerifyBtnDisable] = useState(true);
+
+  useEffect(() => {
+    return () => {
+      clearInterval(timer.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isVerify) {
+      limitTime.current = VERIFY_TIME;
+      setCountDown(parseSecond(limitTime.current));
+      timer.current = setInterval(() => {
+        limitTime.current -= 1;
+        setCountDown(parseSecond(limitTime.current));
+      }, 1000);
+    }
+  }, [isVerify]);
+
+  useEffect(() => {
+    if (limitTime.current === 0) {
+      clearInterval(timer.current);
+      setIsVerify(false);
+    }
+  }, [countDown]);
 
   useEffect(() => {
     const { name, email } = values;
@@ -59,16 +95,22 @@ function RegisterFormStage1({ values, errors, touched, onChange, onBlur }) {
       ) : (
         <S.FormStageInputContainer>
           <S.InputLabel>이메일 인증</S.InputLabel>
-          <S.StageInput
-            name='verify'
-            size='sm'
-            fullWidth
-            value={verify}
-            onChange={onChange}
-            onBlur={onBlur}
-            error={!!(touched.email && errors.verify)}
-            helperText={errors.verify}
-          />
+          <S.EmailVerifyInputContainer>
+            <S.StageInput
+              name='verify'
+              size='sm'
+              fullWidth
+              value={verify}
+              onChange={onChange}
+              onBlur={onBlur}
+              error={!!(touched.verify && errors.verify)}
+              helperText={errors.verify}
+            />
+            <S.Timer>{`${String(countDown.min).padStart(2, '0')}:${String(countDown.sec).padStart(
+              2,
+              '0'
+            )}`}</S.Timer>
+          </S.EmailVerifyInputContainer>
         </S.FormStageInputContainer>
       )}
     </>
