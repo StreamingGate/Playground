7203d74e6aab5b717e656051fa063d81f276bd11
ag502 <ag502@naver.com>
2022-01-24 17:21:11 +0900

    design : add AdviseModal Component

--- src/frontend/src/components/feedbacks/Modals/AdviseModal.jsx
+++ src/frontend/src/components/feedbacks/Modals/AdviseModal.jsx
@@ -4,16 +4,21 @@ import * as S from './Modal.style';
 import { modalService } from '@utils/service';
 
 import { Dialog } from '@components/feedbacks';
+import { Button } from '@components/buttons';
 import { Typography } from '@components/cores';
 
 function AdviseModal(args) {
+  const { content } = args;
   const modal = modalService.useModal();
   return (
     <Dialog open={modal.visible}>
       <S.AdviseModalContainer>
         <S.AdviseModalContent>
-          <Typography type='component'>{args.content}</Typography>
+          <Typography type='component'>{content}</Typography>
         </S.AdviseModalContent>
+        <S.AdviseModalAction>
+          <Button onClick={modal.hide}>확인</Button>
+        </S.AdviseModalAction>
       </S.AdviseModalContainer>
     </Dialog>
   );
