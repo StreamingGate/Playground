0b819c7681cb424a039a779ce4837377fc384c6b
ag502 <ag502@naver.com>
2022-01-24 18:50:35 +0900

    fix : modify API url

--- src/frontend/src/utils/hook/query/useUserRegister.js
+++ src/frontend/src/utils/hook/query/useUserRegister.js
@@ -1,22 +1,21 @@
 import { useMutation } from 'react-query';
 import axios from 'axios';
 
-// /users
 const postUserRegister = async values => {
-  const { data } = await axios.post(`${process.env.REACT_APP_API}/user-service/users`, { values });
+  const { data } = await axios.post(`${process.env.REACT_APP_API}/user-service/users`, {
+    ...values,
+  });
   return data;
 };
 
-// /users/mail?code=#
 const postVerifyCode = async code => {
   const { data } = await axios.get(`${process.env.REACT_APP_API}/user-service/email?code=${code}`);
   return data;
 };
 
-// /nickname?nickname={nickname}
 const postVerifyNickName = async nickName => {
   const { data } = await axios.get(
-    `${process.env.REACT_APP_API}/user-service/nickname?nickName=${nickName}`
+    `${process.env.REACT_APP_API}/user-service/nickname?nickname=${nickName}`
   );
   return data;
 };
