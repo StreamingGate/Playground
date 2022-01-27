14a18f3200b635a7bfaee74fe787b77934c58b6e
chaehui seo <sch0991@naver.com>
2022-01-27 15:35:39 +0900

    design : improve chat list UI

--- src/ios/PlayGround/PlayGround/Presentation/Video/Chat/ChattingViewController.swift
+++ src/ios/PlayGround/PlayGround/Presentation/Video/Chat/ChattingViewController.swift
@@ -37,6 +37,7 @@ class ChattingViewController: UIViewController {
         titleLabel.font = UIFont.Component
         viewerLabel.font = UIFont.caption
         viewerLabel.textColor = UIColor.customDarkGray
+        tableView.contentInset = UIEdgeInsets(top: 15, left: 0, bottom: 0, right: 0)
     }
     
     func bingViewModelData() {
