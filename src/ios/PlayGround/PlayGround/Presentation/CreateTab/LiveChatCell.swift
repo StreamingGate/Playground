7aaef27bff799e53ee1117867f14ca9738eeda3b
chaehui seo <sch0991@naver.com>
2022-01-28 16:04:05 +0900

    feat : add chat-service to broadcaster's view

--- src/ios/PlayGround/PlayGround/Presentation/CreateTab/LiveChatCell.swift
+++ src/ios/PlayGround/PlayGround/Presentation/CreateTab/LiveChatCell.swift
@@ -13,12 +13,14 @@ class LiveChatCell: UITableViewCell {
     @IBOutlet weak var nicknameLabel: UILabel!
     @IBOutlet weak var contentLabel: UILabel!
     
-    func setupUI() {
+    func setupUI(info : ChatData) {
         profileImageView.backgroundColor = UIColor.placeHolder
         profileImageView.layer.cornerRadius = 20
         nicknameLabel.font = UIFont.Tab
         nicknameLabel.textColor = UIColor.white
         contentLabel.textColor = UIColor.white
         contentLabel.font = UIFont.Component
+        contentLabel.text = info.message
+        nicknameLabel.text = info.nickname
     }
 }
