dba937109ec5ee950c9bcdd6d436c037f4f6d295
kimhanui <cocacola2017@naver.com>
2022-01-28 17:17:14 +0900

    fix : remove @EnableDiscoveryClient, dependency

--- src/server/chat-service/src/main/java/com/example/chatservice/ChatServiceApplication.java
+++ src/server/chat-service/src/main/java/com/example/chatservice/ChatServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ChatServiceApplication {
 
 	public static void main(String[] args) {
