ad6b566339300a390a2374dd16c8e512e915c57e
ag502 <ag502@naver.com>
2022-01-31 01:29:16 +0900

    fix : HomePage storybook

--- src/frontend/src/pages/Home/HomePage/HomePage.stories.jsx
+++ src/frontend/src/pages/Home/HomePage/HomePage.stories.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+import { MainLayoutContext } from '@utils/context';
+
+import { MainLayout } from '@components/layouts';
 import HomePage from './HomePage';
 
 export default {
   title: 'Page/Home',
   component: HomePage,
+  decorators: [
+    (Story, rest) => {
+      const { args } = rest;
+      console.log(args);
+      return (
+        <Router>
+          <MainLayoutContext.Provider value={{ sideNavState: { open: true, backdrop: false } }}>
+            <MainLayout>
+              <Story />
+            </MainLayout>
+          </MainLayoutContext.Provider>
+        </Router>
+      );
+    },
+  ],
   parameters: {
     layout: 'fullscreen',
     controls: { hideNoControlsWarning: true },
+  },
+  args: {
+    ctx: { sideNavState: { open: true, backdrop: false } },
   },
 };
 
