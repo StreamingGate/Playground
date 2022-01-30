68a33012d44d6ddb0b9fe271d670fee25fd1e542
ag502 <ag502@naver.com>
2022-01-31 01:09:30 +0900

    feat : apply SlideCategory in HomePage

--- src/frontend/src/pages/Home/HomePage/HomePage.jsx
+++ src/frontend/src/pages/Home/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import * as S from './HomePage.sytle';
 import ThumbNailDummy from '@assets/image/ThumbNailDummy.jpg';
 
+import CategorySlider from '../CategorySlider/CategorySlider';
 import { VideoOverview } from '@components/videos';
 
 const dummyData = [
@@ -48,15 +49,144 @@ const dummyData = [
     viewCount: 1,
     createdAt: '1시간 전',
   },
+  {
+    id: 6,
+    thumbNailSrc: ThumbNailDummy,
+    title: '마크툽(MAKTUB) X 마라는대로 - 찰나가 영원이 될 때 (The Eternal Moment) COVER',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 7,
+    thumbNailSrc: ThumbNailDummy,
+    title: '[디지몬] 당신이 몰랐던 임프몬(베르제브몬)의 슬픈 이야기 (feat.레오몬)',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 8,
+    thumbNailSrc: ThumbNailDummy,
+    title:
+      '해리포터 가장 강력한 능력을 지닌 절대강자 Top 30 - 2부 + 영화 속 소름돋는 메세지와 해석',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 9,
+    thumbNailSrc: ThumbNailDummy,
+    title: 'Harry Potter 20th Anniversary: Return to Hogwarts | Official Trailer | HBO Max',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 10,
+    thumbNailSrc: ThumbNailDummy,
+    title:
+      'Harry Potter and the Deathly Hallows: Part 1 (4/5) Movie CLIP - Escape From Malfoy Manor (2010) HD',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 11,
+    thumbNailSrc: ThumbNailDummy,
+    title: '마크툽(MAKTUB) X 마라는대로 - 찰나가 영원이 될 때 (The Eternal Moment) COVER',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 12,
+    thumbNailSrc: ThumbNailDummy,
+    title: '[디지몬] 당신이 몰랐던 임프몬(베르제브몬)의 슬픈 이야기 (feat.레오몬)',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 13,
+    thumbNailSrc: ThumbNailDummy,
+    title:
+      '해리포터 가장 강력한 능력을 지닌 절대강자 Top 30 - 2부 + 영화 속 소름돋는 메세지와 해석',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 14,
+    thumbNailSrc: ThumbNailDummy,
+    title: 'Harry Potter 20th Anniversary: Return to Hogwarts | Official Trailer | HBO Max',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 15,
+    thumbNailSrc: ThumbNailDummy,
+    title:
+      'Harry Potter and the Deathly Hallows: Part 1 (4/5) Movie CLIP - Escape From Malfoy Manor (2010) HD',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 16,
+    thumbNailSrc: ThumbNailDummy,
+    title: '마크툽(MAKTUB) X 마라는대로 - 찰나가 영원이 될 때 (The Eternal Moment) COVER',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 17,
+    thumbNailSrc: ThumbNailDummy,
+    title: '[디지몬] 당신이 몰랐던 임프몬(베르제브몬)의 슬픈 이야기 (feat.레오몬)',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 18,
+    thumbNailSrc: ThumbNailDummy,
+    title:
+      '해리포터 가장 강력한 능력을 지닌 절대강자 Top 30 - 2부 + 영화 속 소름돋는 메세지와 해석',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 19,
+    thumbNailSrc: ThumbNailDummy,
+    title: 'Harry Potter 20th Anniversary: Return to Hogwarts | Official Trailer | HBO Max',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
+  {
+    id: 20,
+    thumbNailSrc: ThumbNailDummy,
+    title:
+      'Harry Potter and the Deathly Hallows: Part 1 (4/5) Movie CLIP - Escape From Malfoy Manor (2010) HD',
+    userName: 'test',
+    viewCount: 1,
+    createdAt: '1시간 전',
+  },
 ];
 
 function HomePage() {
   return (
-    <S.HomePageContainer>
-      {dummyData.map(videoInfo => (
-        <VideoOverview key={videoInfo.id} videoInfo={videoInfo} />
-      ))}
-    </S.HomePageContainer>
+    <>
+      <CategorySlider />
+      <S.HomePageContainer>
+        {dummyData.map(videoInfo => (
+          <VideoOverview key={videoInfo.id} videoInfo={videoInfo} />
+        ))}
+      </S.HomePageContainer>
+    </>
   );
 }
 
