196313b4285a9a8ac826697898770d847aaaee38
ag502 <ag502@naver.com>
2022-01-30 22:52:18 +0900

    design : add CategorySlider moving arrow

--- src/frontend/src/pages/Home/CategorySlider/CategorySlider.jsx
+++ src/frontend/src/pages/Home/CategorySlider/CategorySlider.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import * as S from './CategorySlider.style';
+import { MainLayoutContext } from '@utils/context';
+
+import { ChipButton } from '@components/buttons';
+
+const dummyCateogry = [
+  { id: 1, category: '전체' },
+  { id: 2, category: '믹스' },
+  { id: 3, category: '음악' },
+  { id: 13, category: '게임' },
+  { id: 4, category: 'K-POP' },
+  { id: 5, category: '교육' },
+  { id: 6, category: '애니메이션' },
+  { id: 7, category: '실시간' },
+  { id: 8, category: '영화' },
+  { id: 9, category: '액션 어드벤처 게임' },
+  { id: 10, category: '반려동물' },
+  { id: 11, category: '최근에 업로드 된 영상' },
+  { id: 12, category: 'This is Test' },
+];
 
 function CategorySlider() {
-  return <S.CategorySliderContainer>test</S.CategorySliderContainer>;
+  const { sideNavState } = useContext(MainLayoutContext);
+  return (
+    <S.CategorySliderContainer sideNavState={sideNavState}>
+      <S.ArrowLeftIcon />
+      <S.CategoryContainer>
+        {dummyCateogry.map(({ id, category }) => (
+          <ChipButton key={id} content={category} />
+        ))}
+      </S.CategoryContainer>
+      <S.ArrowRightIcon />
+    </S.CategorySliderContainer>
+  );
 }
 
 export default CategorySlider;
--- src/frontend/src/pages/Home/CategorySlider/CategorySlider.style.js
+++ src/frontend/src/pages/Home/CategorySlider/CategorySlider.style.js
@@ -2,15 +2,20 @@ import styled, { css } from 'styled-components';
 
 import { breakPoint } from '@utils/constant';
 
+import { ArrowLeft, ArrowRight } from '@components/cores';
+
 const { queries } = breakPoint;
 
 export const CategorySliderContainer = styled.div`
+  --category-slider-padding: 15px;
+  display: flex;
+  align-items: center;
   position: fixed;
   left: 0;
   right: var(--side-friend-list-width);
   z-index: 1;
   height: 60px;
-  padding: 15px 0px;
+  padding: var(---category-slider-padding) 0px;
   background-color: #ffffff;
   border-top: ${({ theme }) => `1px solid ${theme.colors.separator}`};
   border-bottom: ${({ theme }) => `1px solid ${theme.colors.separator}`};
@@ -34,4 +39,16 @@ export const CategoryContainer = styled.div`
   margin: 0 auto;
   white-space: nowrap;
   overflow-x: hidden;
+`;
+
+export const ArrowLeftIcon = styled(ArrowLeft)`
+  position: absolute;
+  cursor: pointer;
+`;
+
+export const ArrowRightIcon = styled(ArrowRight)`
+  position: absolute;
+  top: var(--category-slider-padding);
+  right: 0;
+  cursor: pointer;
 `;
