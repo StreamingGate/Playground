1519b78004110ba3456b116046313a3ef1f93959
ag502 <ag502@naver.com>
2022-02-01 01:15:32 +0900

    feat : add FriendModal toggle logic

--- src/frontend/src/components/layouts/SideFriendList/SideFriendList.jsx
+++ src/frontend/src/components/layouts/SideFriendList/SideFriendList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useCallback, useRef } from 'react';
 
 import S from './SideFriendList.style';
 import { MainLayoutContext } from '@utils/context';
@@ -19,10 +19,47 @@ const dummyFriends = [
   { isOnline: false, profileImgSrc: '', name: '차정원' },
   { isOnline: false, profileImgSrc: '', name: '신류진' },
   { isOnline: false, profileImgSrc: '', name: '이이경' },
+  { isOnline: false, profileImgSrc: '', name: 'A' },
+  { isOnline: false, profileImgSrc: '', name: 'B' },
+  { isOnline: false, profileImgSrc: '', name: 'C' },
+  { isOnline: false, profileImgSrc: '', name: 'D' },
+  { isOnline: false, profileImgSrc: '', name: 'E' },
+  { isOnline: false, profileImgSrc: '', name: 'F' },
+  { isOnline: false, profileImgSrc: '', name: 'G' },
+  { isOnline: false, profileImgSrc: '', name: 'H' },
+  { isOnline: false, profileImgSrc: '', name: 'I' },
+  { isOnline: false, profileImgSrc: '', name: 'J' },
 ];
 
 function SideFriendList() {
+  const friendModalRef = useRef(null);
   const { sideFriendState, onToggleSideFriend } = useContext(MainLayoutContext);
+
+  const [selectedFriendIdx, setSelectedFriendIdx] = useState(-1);
+  const [friendBottomPos, setFriendBottomPos] = useState(0);
+  const [isFriendModalToggle, setFriendModalToggle] = useState(false);
+
+  const handleFriendItemClick = idx => e => {
+    const { currentTarget } = e;
+    const friendModalHeight = friendModalRef.current.getBoundingClientRect().height;
+    const currentTargetPos = currentTarget.getBoundingClientRect();
+
+    if (isFriendModalToggle && selectedFriendIdx === idx) {
+      setSelectedFriendIdx(idx);
+      setFriendModalToggle(false);
+    } else {
+      if (idx === dummyFriends.length - 1) {
+        setFriendBottomPos(currentTargetPos.bottom - friendModalHeight);
+      } else {
+        setFriendBottomPos(
+          currentTargetPos.y + currentTargetPos.height / 2 - friendModalHeight / 2
+        );
+      }
+      setFriendModalToggle(true);
+      setSelectedFriendIdx(idx);
+    }
+  };
+
   return (
     <>
       <BackDrop
@@ -33,11 +70,29 @@ function SideFriendList() {
       <S.SideFriendListContainer state={sideFriendState}>
         <S.SideFriendListHeader type='highlightCaption'>친구 목록</S.SideFriendListHeader>
         <S.FriendList>
-          {dummyFriends.map(({ isOnline, profileImgSrc, name }) => (
+          {dummyFriends.map(({ isOnline, profileImgSrc, name }, idx) => (
             // 추후 profileImgSrc로 변경
-            <FriendItem key={name} isOnline={isOnline} profileImgSrc={profileImgSrc} name={name} />
+            <FriendItem
+              key={name}
+              isOnline={isOnline}
+              profileImgSrc={profileImgSrc}
+              name={name}
+              onClick={handleFriendItemClick(idx)}
+            />
           ))}
         </S.FriendList>
+        <S.FriendModalContainer
+          ref={friendModalRef}
+          top={friendBottomPos}
+          isShow={isFriendModalToggle}
+        >
+          <S.FriendAvatar size='lg' />
+          <S.FriendModalRightDiv>
+            <S.FriendModalName type='highlightCaption'>친구이름</S.FriendModalName>
+            <S.FriendVideoName type='caption'>시청 중인 영상제목</S.FriendVideoName>
+            <S.PlayWithFriendBtn>영상 같이 시청하기</S.PlayWithFriendBtn>
+          </S.FriendModalRightDiv>
+        </S.FriendModalContainer>
       </S.SideFriendListContainer>
     </>
   );
--- src/frontend/src/components/layouts/SideFriendList/SideFriendList.style.js
+++ src/frontend/src/components/layouts/SideFriendList/SideFriendList.style.js
@@ -1,6 +1,8 @@
 import styled, { css } from 'styled-components';
 
 import { Typography } from '@components/cores';
+import { Button } from '@components/buttons';
+import { Avatar } from '@components/dataDisplays';
 
 export default {
   SideFriendListContainer: styled.div`
@@ -41,11 +43,47 @@ export default {
   FriendList: styled.ul`
     display: flex;
     flex-direction: column;
+    overflow: auto;
+    /* height: calc(100vh - 2 * var(--head-height)); */
     gap: 15px;
   `,
   FriendItem: styled.li`
     display: flex;
     align-items: center;
+    cursor: pointer;
+  `,
+  FriendModalContainer: styled.div`
+    display: flex;
+    position: fixed;
+    top: ${({ top }) => `${top}px`};
+    width: 230px;
+    margin-left: -260px;
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+    visibility: ${({ isShow }) => (isShow ? 'visible' : 'hidden')};
+    background-color: #ffffff;
+  `,
+  FriendAvatar: styled(Avatar)`
+    margin-right: 8px;
+  `,
+  FriendModalRightDiv: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+  FriendModalName: styled(Typography)`
+    margin-bottom: 2px;
+  `,
+  FriendVideoName: styled(Typography)`
+    margin-bottom: 10px;
+    color: ${({ theme }) => theme.colors.customDarkGray};
+  `,
+  PlayWithFriendBtn: styled(Button)`
+    padding: 5px 25px;
+    color: #ffffff;
+    font-size: ${({ theme }) => theme.fontSizes.highlightCaption};
+    background-color: ${({ theme }) => theme.colors.pgBlue};
+    border-radius: 12px;
   `,
   FriendName: styled(Typography)`
     margin-left: 5px;
