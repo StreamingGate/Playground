a660e56a1e6757a522d10b2e81494172857bca59
chaehui seo <sch0991@naver.com>
2022-01-31 15:35:30 +0900

    chore : delete struct that is not used

--- src/ios/PlayGround/PlayGround/Network/MainService/MainDataModel.swift
+++ src/ios/PlayGround/PlayGround/Network/MainService/MainDataModel.swift
@@ -19,62 +19,6 @@ struct HomeList: Codable {
     }
 }
 
-enum Category: String {
-    case ALL = "전체"
-    case EDU = "교육"
-    case SPORTS = "스포츠"
-    case KPOP = "K-POP"
-}
-
-
-struct LiveRoom: Codable {
-    let id: Int
-    let title: String
-    let hostNickname: String
-    let fileLink: String?
-    let thumbnail: String
-    let category: String
-    let createdAt: String
-    let streamingId: String?
-    let chatRoomId: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case hostNickname
-        case fileLink
-        case thumbnail
-        case category
-        case createdAt
-        case streamingId
-        case chatRoomId
-    }
-}
-
-
-struct Video: Codable {
-    let id: Int
-    let title: String
-    let uploaderNickname: String
-    let hits: Int
-    let fileLink: String?
-    let thumbnail: String
-    let category: String
-    let createdAt: String
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case title
-        case uploaderNickname
-        case hits
-        case fileLink
-        case thumbnail
-        case category
-        case createdAt
-    }
-}
-
-
 struct GeneralVideo: Codable {
     let id: Int
     let title: String
