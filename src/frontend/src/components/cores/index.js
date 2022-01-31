236caf504761a7a44d2d7c5c4231431eb707de72
ag502 <ag502@naver.com>
2022-01-31 12:49:45 +0900

    chore : add AddFullCircle Svg Icon

--- src/frontend/src/components/cores/index.js
+++ src/frontend/src/components/cores/index.js
@@ -21,3 +21,4 @@ export { default as Report } from './Icons/Report';
 export { default as ScrollDown } from './Icons/ScrollDown';
 export { default as ArrowLeft } from './Icons/ArrowLeft';
 export { default as ArrowRight } from './Icons/ArrowRight';
+export { default as AddFullCircle } from './Icons/AddFullCircle';
