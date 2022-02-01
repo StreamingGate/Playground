a9ff659781dc09d8b77427448d99d507ad1497f3
chaehui seo <sch0991@naver.com>
2022-02-01 21:47:44 +0900

    feat : add play viewmodel

---
+++ src/ios/PlayGround/PlayGround/Presentation/Video/Play/PlayViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayViewModel.swift
+//  PlayGround
+//
+//  Created by chuiseo-MN on 2022/02/01.
+//
+
+import Foundation
+import UIKit
+
+class PlayViewModel {
+    var currentInfo: GeneralVideo?
+}
