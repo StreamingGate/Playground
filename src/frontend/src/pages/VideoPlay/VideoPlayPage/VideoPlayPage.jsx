c4b26edd37789c945e526bf0c8de220aeb5d0a5e
ag502 <ag502@naver.com>
2022-02-02 17:57:08 +0900

    feat : process m3u8 video format

--- src/frontend/src/pages/VideoPlay/VideoPlayPage/VideoPlayPage.jsx
+++ src/frontend/src/pages/VideoPlay/VideoPlayPage/VideoPlayPage.jsx
@@ -1,16 +1,26 @@
-import React, { memo } from 'react';
+import React, { memo, useRef, useEffect } from 'react';
+import Hls from 'hls.js';
 
 import * as S from './VideoPlayPage.style';
-import DummyVideo from '@assets/video/DummyVideo.mp4';
 
 import ChatRoom from '@pages/VideoPlay/ChatRoom/ChatRoom';
 import VideoMetaData from '@pages/VideoPlay/VideoMetaData/VideoMetaData';
 
 function VideoPlayPage() {
+  const videoPlayerRef = useRef(null);
+
+  useEffect(() => {
+    const videoSrc =
+      'https://s3.ap-northeast-2.amazonaws.com/sg.playground/video/output/momo_trans.m3u8';
+    const hls = new Hls();
+    hls.loadSource(videoSrc);
+    hls.attachMedia(videoPlayerRef.current);
+  }, []);
+
   return (
     <S.VideoPlayPageContainer>
       <S.PlayerContainer>
-        <S.VideoPlayer src={DummyVideo} controls>
+        <S.VideoPlayer controls ref={videoPlayerRef}>
           Video Load Fail
         </S.VideoPlayer>
       </S.PlayerContainer>
