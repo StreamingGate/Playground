20a5bf62a7daaf0080562587b71e332a1f08dc23
ag502 <ag502@naver.com>
2022-02-02 23:27:21 +0900

    feat : add next page change logic

--- src/frontend/src/utils/hook/query/useMainVideoList.js
+++ src/frontend/src/utils/hook/query/useMainVideoList.js
@@ -2,20 +2,17 @@ import { useInfiniteQuery } from 'react-query';
 import axios from '@utils/axios';
 
 const getMainVideoList = async ({ pageParam = 0 }) => {
-  const data = await axios.get(`/main-service/list?page=${pageParam}&size=5&category=ALL`);
+  const data = await axios.get(`/main-service/list?page=${pageParam}&size=3&category=ALL`);
   return data;
 };
 
 export default function useMainVideoList() {
   return useInfiniteQuery('main', getMainVideoList, {
     getNextPageParam: (lastPage, pages) => {
-      // console.log('~~~~~lastpage~~~~~');
-      // console.log(lastPage);
-      // console.log('~~~~~~~~~~~~~~~~~~');
-      // console.log('~~~~~~~pages~~~~~~');
-      // console.log(pages);
-      // console.log('~~~~~~~~~~~~~~~~~~');
-      return lastPage.nextCursor;
+      if (lastPage.liveRooms.length === 0 && lastPage.videos.length === 0) {
+        return undefined;
+      }
+      return pages.length;
     },
   });
 }
