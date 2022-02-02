14f8ecf4bee1e6c9e2beac00a392fda8c7c16b0d
ag502 <ag502@naver.com>
2022-02-03 02:09:07 +0900

    feat : add query key

--- src/frontend/src/utils/hook/query/useMainVideoList.js
+++ src/frontend/src/utils/hook/query/useMainVideoList.js
@@ -1,13 +1,15 @@
 import { useInfiniteQuery } from 'react-query';
 import axios from '@utils/axios';
 
-const getMainVideoList = async ({ pageParam = 0 }) => {
-  const data = await axios.get(`/main-service/list?page=${pageParam}&size=3&category=ALL`);
+const getMainVideoList = async ({ pageParam = 0, queryKey }) => {
+  const data = await axios.get(
+    `/main-service/list?page=${pageParam}&size=3&category=${queryKey[1]}`
+  );
   return data;
 };
 
-export default function useMainVideoList() {
-  return useInfiniteQuery('main', getMainVideoList, {
+export default function useMainVideoList(category) {
+  return useInfiniteQuery(['main', category], getMainVideoList, {
     refetchOnWindowFocus: false,
     getNextPageParam: (lastPage, pages) => {
       if (lastPage.liveRooms.length === 0 && lastPage.videos.length === 0) {
