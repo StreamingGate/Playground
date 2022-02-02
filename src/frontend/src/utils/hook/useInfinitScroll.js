408d4b99d0b59f4dd4d3490b4e73fadd060a48c2
ag502 <ag502@naver.com>
2022-02-03 00:54:54 +0900

    fix : infinite scroll

--- src/frontend/src/utils/hook/useInfinitScroll.js
+++ src/frontend/src/utils/hook/useInfinitScroll.js
@@ -1,18 +1,10 @@
-import { useEffect, useRef, useCallback } from 'react';
-
 export default function useInifinitScroll(callback, option) {
-  const observer = useRef(null);
-
-  const handleObserver = useCallback(entries => {
+  const handleObserver = entries => {
     const target = entries[0];
-    if (target.isIntersecting) {
+    if (target.isIntersecting && callback) {
       callback();
     }
-  }, []);
+  };
 
-  useEffect(() => {
-    observer.current = new IntersectionObserver(handleObserver, option);
-  }, []);
-
-  return observer.current;
+  return new IntersectionObserver(handleObserver, option);
 }
