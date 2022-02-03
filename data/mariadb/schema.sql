98aa156e85c0ba0780dfd302f17cd3afc983cfa5
kimhanui <cocacola2017@naver.com>
2022-02-03 15:30:28 +0900

    feat : renamed column chat_room_uuid -> chat_uuid

--- data/mariadb/schema.sql
+++ data/mariadb/schema.sql
@@ -1,8 +1,7 @@
 create table friend
 (
     users_id  bigint not null,
-    friend_id bigint not null,
-    primary key (users_id, friend_id)
+    friend_id bigint not null
 ) engine=InnoDB;
 create table friend_wait
 (
@@ -13,27 +12,27 @@ create table friend_wait
     users_id             bigint,
     primary key (id)
 ) engine=InnoDB;
-create table live_room
+create table live
 (
-    id            bigint   not null auto_increment,
-    category      varchar(10),
-    chat_room_id  varchar(255),
-    content       varchar(5000),
-    created_at    datetime,
-    host_nickname varchar(255),
-    like_cnt      integer  not null,
-    report_cnt    smallint not null,
-    streaming_id  varchar(255),
-    thumbnail     TEXT,
-    title         varchar(100),
-    users_id      bigint,
+    id             bigint   not null auto_increment,
+    category       varchar(10),
+    chat_uuid      varchar(36),
+    content        varchar(5000),
+    created_at     datetime,
+    host_nickname  varchar(255),
+    like_cnt       integer  not null,
+    report_cnt     smallint not null,
+    streaming_uuid varchar(36),
+    thumbnail      TEXT,
+    title          varchar(100),
+    users_id       bigint,
     primary key (id)
 ) engine=InnoDB;
 create table live_viewer
 (
     id             bigint not null auto_increment,
     disliked       bit    not null,
-    last_viewed_at datetime,
+    last_viewed_at bigint,
     liked          bit    not null,
     liked_at       datetime,
     live_room_id   bigint,
@@ -80,6 +79,7 @@ create table video
 (
     id                bigint   not null auto_increment,
     category          varchar(10),
+    chat_uuid         varchar(36),
     content           varchar(5000),
     created_at        datetime,
     hits              integer  not null,
@@ -88,6 +88,7 @@ create table video
     thumbnail         TEXT,
     title             varchar(100),
     uploader_nickname varchar(255),
+    uuid              varchar(36),
     metadata_id       bigint,
     users_id          bigint,
     primary key (id)
@@ -104,6 +105,7 @@ create table viewed_history
     video_id        bigint,
     primary key (id)
 ) engine=InnoDB;
+alter table users drop index UK_6dotkott2kjsp8vw4d0m25fb7;
 alter table users
     add constraint UK_6dotkott2kjsp8vw4d0m25fb7 unique (email);
 alter table friend
@@ -112,10 +114,10 @@ alter table friend
     add constraint FK77hwidnt50k76utjsr02f15dx foreign key (users_id) references users (id);
 alter table friend_wait
     add constraint FK5m461lrdbrvaajjaj3v0upu1h foreign key (users_id) references users (id);
-alter table live_room
-    add constraint FK6vnxsrdcyyl32b00uvr4mdt2e foreign key (users_id) references users (id);
+alter table live
+    add constraint FK92l0bpkdmody39uwicxkcvul2 foreign key (users_id) references users (id);
 alter table live_viewer
-    add constraint FK6swae88o8jsovsq1hbk50kvg6 foreign key (live_room_id) references live_room (id);
+    add constraint FK4wmoyubomxmidp6hqd21ujiud foreign key (live_room_id) references live (id);
 alter table live_viewer
     add constraint FKovjr1pogd7rjlfah9i13dm2o5 foreign key (user_id) references users (id);
 alter table notification
