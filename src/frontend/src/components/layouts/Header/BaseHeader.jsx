467fc6ef31d6d816c342e666993eeca570bbf1bc
ag502 <ag502@naver.com>
2022-02-03 22:27:23 +0900

    feat : apply DeleteFriendModal in Header

--- src/frontend/src/components/layouts/Header/BaseHeader.jsx
+++ src/frontend/src/components/layouts/Header/BaseHeader.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext, useState } from 'react';
 
 import * as S from './Header.style';
+import { modalService } from '@utils/service';
 import { HeaderContext, MainLayoutContext } from '@utils/context';
 
 import { IconButton } from '@components/buttons';
@@ -15,6 +16,7 @@ import {
   MyVideo,
   LiveStreaming,
 } from '@components/cores';
+import { AcceptFriendModal, DeleteFriendModal } from '@components/feedbacks/Modals';
 import SearchForm from './SearchForm';
 
 const dummyFriends = [
@@ -52,6 +54,32 @@ function BaseHeader() {
     };
   }, [toggleState]);
 
+  const handleAddVideoModalClick = e => {
+    e.stopPropagation();
+  };
+
+  const handleAlarmModalClick = e => {
+    e.stopPropagation();
+    const { target } = e;
+    if (target.tagName === 'BUTTON') {
+      modalService.show(AcceptFriendModal);
+    }
+  };
+
+  const handleProfileModalClick = e => {
+    e.stopPropagation();
+    const { target } = e;
+    const button = target.closest('button');
+
+    if (!button) return;
+    const [buttonId, friendIdx] = button.id.split('_');
+
+    if (buttonId === 'friendDelete') {
+      const seletedName = dummyFriends[Number(friendIdx)].name;
+      modalService.show(DeleteFriendModal, { friendName: seletedName });
+    }
+  };
+
   return (
     <>
       <S.HeaderLeftDiv>
@@ -69,7 +97,7 @@ function BaseHeader() {
           <IconButton name='addVideo' onClick={onToggleModal}>
             {modalState.addVideo ? <AddFullCircle /> : <AddEmptyCircle />}
           </IconButton>
-          <S.AddVideoDropdownContainer>
+          <S.AddVideoDropdownContainer onClick={handleAddVideoModalClick}>
             {modalState.addVideo && (
               <S.AddVideoMenus>
                 <S.AddVideoMenu>
@@ -88,7 +116,7 @@ function BaseHeader() {
           <IconButton name='alarm' onClick={onToggleModal}>
             {modalState.alarm ? <FullAlarm /> : <Alarm />}
           </IconButton>
-          <S.AlarmDropdownContainer>
+          <S.AlarmDropdownContainer onClick={handleAlarmModalClick}>
             {modalState.alarm && (
               <S.AlarmDropdown>
                 <S.AlarmTitle>
@@ -113,16 +141,18 @@ function BaseHeader() {
         </div>
         <div>
           <S.HeaderAvatar name='profile' onClick={onToggleModal} />
-          <S.ProfileDropdownContainer>
+          <S.ProfileDropdownContainer onClick={handleProfileModalClick}>
             {modalState.profile && (
               <S.ProfileDropdown>
                 <S.UserProfileInfo>
                   <S.UserAvartar size='xl' />
                   <S.UserName>
                     <Typography type='content'>닉네임</Typography>
-                    <S.ModifyUserInfoBtn variant='text'>정보 수정</S.ModifyUserInfoBtn>
+                    <S.ModifyUserInfoBtn variant='text' id='modifyProfile'>
+                      정보 수정
+                    </S.ModifyUserInfoBtn>
                   </S.UserName>
-                  <S.LogoutButton variant='text'>
+                  <S.LogoutButton variant='text' id='logout'>
                     <S.LogoutBtnIcon />
                     로그아웃
                   </S.LogoutButton>
@@ -130,11 +160,13 @@ function BaseHeader() {
                 <S.FriendListContainer>
                   <S.FriendListTitle type='highlightCaption'>친구목록</S.FriendListTitle>
                   <S.FriendList>
-                    {dummyFriends.map(({ id, name }) => (
+                    {dummyFriends.map(({ id, name }, idx) => (
                       <S.FriendInfo key={id}>
                         <S.FriendAvatar tyep='sm' />
                         <S.FriendName type='caption'>{name}</S.FriendName>
-                        <S.FriendDeleteBtn variant='text'>삭제</S.FriendDeleteBtn>
+                        <S.FriendDeleteBtn id={`friendDelete_${idx}`} variant='text'>
+                          삭제
+                        </S.FriendDeleteBtn>
                       </S.FriendInfo>
                     ))}
                   </S.FriendList>
