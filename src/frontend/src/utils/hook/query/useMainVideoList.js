c0d5fcd3f520005fffe348e9781c52b39240e9ce
ag502 <ag502@naver.com>
2022-02-03 13:12:23 +0900

    refactor : modify api address and format

--- src/frontend/src/utils/hook/query/useMainVideoList.js
+++ src/frontend/src/utils/hook/query/useMainVideoList.js
@@ -1,9 +1,10 @@
 import { useInfiniteQuery } from 'react-query';
 import axios from '@utils/axios';
 
-const getMainVideoList = async ({ pageParam = 0, queryKey }) => {
+const getMainVideoList = async ({ pageParam = { lastVideo: -1, lastLive: -1 }, queryKey }) => {
+  const { lastVideo, lastLive } = pageParam;
   const data = await axios.get(
-    `/main-service/list?page=${pageParam}&size=3&category=${queryKey[1]}`
+    `/main-service/list?last-video=${lastVideo}&last-live=${lastLive}&size=3&category=${queryKey[1]}`
   );
   return data;
 };
@@ -11,11 +12,17 @@ const getMainVideoList = async ({ pageParam = 0, queryKey }) => {
 export default function useMainVideoList(category) {
   return useInfiniteQuery(['main', category], getMainVideoList, {
     refetchOnWindowFocus: false,
-    getNextPageParam: (lastPage, pages) => {
-      if (lastPage.liveRooms.length === 0 && lastPage.videos.length === 0) {
+    getNextPageParam: lastPage => {
+      const { liveRooms, videos } = lastPage;
+
+      if (liveRooms.length === 0 && videos.length === 0) {
         return undefined;
       }
-      return pages.length;
+
+      return {
+        lastVideo: videos[videos.length - 1]?.id,
+        lastLive: liveRooms[liveRooms.length - 1]?.id,
+      };
     },
   });
 }
