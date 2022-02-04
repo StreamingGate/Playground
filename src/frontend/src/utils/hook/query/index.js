e5c86db09382ce5d10e48fede07227919e3cf6ee
ag502 <ag502@naver.com>
2022-02-04 15:12:33 +0900

    feat : add get friend list api with react-query

--- src/frontend/src/utils/hook/query/index.js
+++ src/frontend/src/utils/hook/query/index.js
@@ -1,3 +1,4 @@
 export { default as useUserRegister } from './useUserRegister';
 export { default as useLogin } from './useLogin';
 export { default as useMainVideoList } from './useMainVideoList';
+export { default as useFriendList } from './useFriendList';
