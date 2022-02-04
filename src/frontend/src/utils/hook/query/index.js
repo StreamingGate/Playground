87f12b8abd31e32010da71b31723cd5e48b5c8a8
ag502 <ag502@naver.com>
2022-02-04 16:38:31 +0900

    feat : add delete friend react query

--- src/frontend/src/utils/hook/query/index.js
+++ src/frontend/src/utils/hook/query/index.js
@@ -2,3 +2,4 @@ export { default as useUserRegister } from './useUserRegister';
 export { default as useLogin } from './useLogin';
 export { default as useMainVideoList } from './useMainVideoList';
 export { default as useFriendList } from './useFriendList';
+export { default as useDeleteFriend } from './useDeleteFriend';
