aa649b45e60798ce84ec24f4b33352027303cb03
ag502 <ag502@naver.com>
2022-02-04 17:04:15 +0900

    feat : add get friend req list react query

--- src/frontend/src/utils/hook/query/index.js
+++ src/frontend/src/utils/hook/query/index.js
@@ -3,3 +3,4 @@ export { default as useLogin } from './useLogin';
 export { default as useMainVideoList } from './useMainVideoList';
 export { default as useFriendList } from './useFriendList';
 export { default as useDeleteFriend } from './useDeleteFriend';
+export { default as useGetFriendReqList } from './useGetFriendReqList';
---
+++ src/frontend/src/utils/hook/query/useGetFriendReqList.js
@@ -0,0 +1,11 @@
+import { useQuery } from 'react-query';
+import axios from '@utils/axios';
+
+const getFriendReqList = async uuid => {
+  const data = await axios.get(`/main-service/friends/manage/${uuid}`);
+  return data;
+};
+
+export default function useGetFriendReqList(uuid) {
+  return useQuery('friends_req', () => getFriendReqList(uuid));
+}
