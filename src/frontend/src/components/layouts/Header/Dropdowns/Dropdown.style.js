2c92c082427ef8b43eeb3df1abb4b561b14af16d
ag502 <ag502@naver.com>
2022-02-04 21:01:23 +0900

    refactor : separate Dropdown component style

--- src/frontend/src/components/layouts/Header/Dropdowns/Dropdown.style.js
+++ src/frontend/src/components/layouts/Header/Dropdowns/Dropdown.style.js
@@ -4,6 +4,58 @@ import { Logout, Typography } from '@components/cores';
 import { Button } from '@components/buttons';
 import { Avatar } from '@components/dataDisplays';
 
+export const AlarmDropdownContainer = styled.div`
+  position: relative;
+  margin-left: -300px;
+`;
+
+export const AlarmDropdown = styled.div`
+  position: absolute;
+  width: 100%;
+  background-color: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const AlarmTitle = styled.div`
+  padding: 15px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.separator};
+`;
+
+export const AlarmBody = styled.div``;
+
+export const AcceptFriendBtnContainer = styled.div`
+  padding: 15px 25px 20px;
+`;
+
+export const AcceptFriendBtn = styled(Button)`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.pgBlue};
+  font-size: ${({ theme }) => theme.fontSizes.content};
+  border-radius: 15px;
+`;
+
+export const AlarmList = styled.ul`
+  height: 350px;
+  overflow: auto;
+`;
+
+export const AlarmInfo = styled.li`
+  display: flex;
+  margin: 0px 0px 20px 20px;
+  align-items: center;
+`;
+
+export const AlarmAvartar = styled(Avatar)`
+  flex-shrink: 0;
+  margin-right: 10px;
+`;
+
+export const AlarmContent = styled(Typography)`
+  & > span {
+    color: ${({ theme }) => theme.colors.placeHolder};
+  }
+`;
+
 export const ProfileDropdownContainer = styled.div`
   position: relative;
   margin-left: -300px;
