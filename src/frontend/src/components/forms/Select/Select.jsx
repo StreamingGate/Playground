9afb2b4415001c8807a90f30cda40bda8d5d65eb
ag502 <ag502@naver.com>
2022-02-06 01:11:53 +0900

    feat : add onChange props in Select component

--- src/frontend/src/components/forms/Select/Select.jsx
+++ src/frontend/src/components/forms/Select/Select.jsx
@@ -3,12 +3,25 @@ import PropTypes from 'prop-types';
 
 import * as S from './Select.style';
 
-function Select({ children }) {
-  return <S.Select>{children}</S.Select>;
+function Select({ className, children, onChange, value }) {
+  return (
+    <S.Select className={className} onChange={onChange} value={value}>
+      {children}
+    </S.Select>
+  );
 }
 
 Select.propTypes = {
+  className: PropTypes.string,
+  onChange: PropTypes.func,
   children: PropTypes.arrayOf(PropTypes.element).isRequired,
+  value: PropTypes.string,
+};
+
+Select.defaultProps = {
+  className: '',
+  onChange: undefined,
+  value: '',
 };
 
 export default Select;
