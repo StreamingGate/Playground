3b971f27393771bcf2773b016f176eb5ba657e8a
kmswlee <wodndl895@kookmin.ac.kr>
2022-02-06 17:10:17 +0900

    feat : add room-service and refactor login api on user-service

--- src/server/room-service/src/main/java/com/example/roomservice/exceptionHandler/customexception/CustomRoomException.java
+++ src/server/room-service/src/main/java/com/example/roomservice/exceptionHandler/customexception/CustomRoomException.java
@@ -2,9 +2,6 @@ package com.example.roomservice.exceptionHandler.customexception;
 
 import lombok.Getter;
 
-/**
- * 다른 서비스와 관련된 에러라도 여기선 커스텀 예외는 CustomMainException.java 하나로 통일한다.
- */
 @Getter
 public class CustomRoomException extends RuntimeException {
 
