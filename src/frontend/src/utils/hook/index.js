ca23e4955414306257e22e602749894abbcc9c07
ag502 <ag502@naver.com>
2022-02-06 18:29:19 +0900

    feat : add media stream custom hook

--- src/frontend/src/utils/hook/index.js
+++ src/frontend/src/utils/hook/index.js
@@ -2,3 +2,4 @@ export { default as useWindowSize } from './useWindowSize';
 export { default as useForm } from './useForm';
 export { default as useSocket } from './useSocket';
 export { default as useInifinitScroll } from './useInfinitScroll';
+export { default as useStreamMedia } from './useStreamMedia';
---
+++ src/frontend/src/utils/hook/useStreamMedia.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+const constraints = { audio: true, video: { width: 1280, height: 720 } };
+
+export default function useStreamMedia(streamPlayerRef) {
+  const [stream, setStream] = useState({ videoTrack: null, audioTrack: null });
+
+  async function getMediaStream() {
+    try {
+      const newStream = await navigator.mediaDevices.getUserMedia(constraints);
+      const videoDom = streamPlayerRef.current;
+
+      videoDom.srcObject = newStream;
+      videoDom.onloadedmetadata = () => {
+        videoDom.play();
+      };
+
+      setStream({
+        videoTrack: newStream.getVideoTracks()[0],
+        audioTrack: newStream.getAudioTracks()[0],
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getMediaStream();
+  }, []);
+
+  return stream;
+}
