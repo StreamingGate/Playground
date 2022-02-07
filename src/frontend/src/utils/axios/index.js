c106ecc7e364a5b8234e347af0fb997361b87ec3
ag502 <ag502@naver.com>
2022-02-07 23:51:24 +0900

    feat : add redirect logic

--- src/frontend/src/utils/axios/index.js
+++ src/frontend/src/utils/axios/index.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import { history } from '@utils/router';
 
 const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_API });
 
 axiosInstance.interceptors.request.use(
   config => {
+    config.headers.Authorization =
+      'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIzMzMzMzMzMy0xMjM0LTEyMzQtMTIzNDU2Nzg5MDEyIiwiZXhwIjoxNjQ0NTAwMDQ4fQ.8tUJ5Akh4TT5Uc2u9RqXtZAbIqSC8fLYputpM0DBr4sSbaPuy6nW7XAwGTuMmM48cScolYPxih-yw3-4AHwwHQ';
     return config;
   },
   error => {
@@ -16,6 +19,10 @@ axiosInstance.interceptors.response.use(
     return response.data;
   },
   error => {
+    if (error.response.status === 401) {
+      history.push('/login');
+      history.go();
+    }
     return Promise.reject(error);
   }
 );
