7bd34353a8e9e03ccab12f4b32b1b0c2661d9dbe
ag502 <ag502@naver.com>
2022-02-10 14:11:30 +0900

    feat : add alert

--- src/frontend/src/pages/Studio/MobileStudioPage/MobileStudioPage.jsx
+++ src/frontend/src/pages/Studio/MobileStudioPage/MobileStudioPage.jsx
@@ -13,6 +13,7 @@ function MobileStudioPage() {
   const streamPlayerRef = useRef(null);
 
   const { roomId, hostId } = useParams();
+  alert(roomId, hostId);
   const { stream, switchCamera, stopStream } = useStreamMedia(streamPlayerRef, 'mobile');
 
   const handleProcessProduce = async peer => {
