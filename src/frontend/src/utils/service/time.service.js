fe3106239c12b4aa7a9596d94467171267989a32
ag502 <ag502@naver.com>
2022-02-10 15:20:21 +0900

    feat : add process video upload time logic

---
+++ src/frontend/src/utils/service/time.service.js
@@ -0,0 +1,30 @@
+const processVideoUploadTime = time => {
+  const now = new Date();
+  const uploadTime = new Date(time);
+
+  const seconds = Math.floor((now - uploadTime) / 1000);
+  let difference = seconds / 31536000;
+
+  if (difference > 1) {
+    return `${Math.floor(difference)}년 전`;
+  }
+
+  difference = seconds / 2592000;
+  if (difference > 1) {
+    return `${Math.floor(difference)}개월 전`;
+  }
+
+  difference = seconds / 86400;
+  if (difference > 1) {
+    return `${Math.floor(difference)}일 전`;
+  }
+
+  difference = seconds / 3600;
+  if (difference > 1) {
+    return `${Math.floor(difference)}시간 전`;
+  }
+
+  return `${Math.floor(difference)}분 전`;
+};
+
+export default { processVideoUploadTime };
