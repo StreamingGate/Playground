2006bf5add17d1964a4c260f41f4bc21a1d74320
kimhanui <cocacola2017@naver.com>
2022-02-11 17:31:50 +0900

    refactor : remove unused file (chat-service)

--- src/server/chat-service/src/main/java/com/example/chatservice/config/RedisConfig.java
+++ src/server/chat-service/src/main/java/com/example/chatservice/config/RedisConfig.java
@@ -9,14 +9,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    // jackson LocalDateTime mapper
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
@@ -25,10 +23,7 @@ public class RedisConfig {
         return mapper;
     }
 
-    /**
-     * redis pub/sub 메시지를 처리하는 listener 설정
-     * pub/sub은 항상 레디스에 데이터가 있는지 확인해야하므로
-     */
+    /* redis pub/sub 메시지를 처리하는 listener 설정 */
     @Bean
     public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
@@ -36,10 +31,7 @@ public class RedisConfig {
         return container;
     }
 
-    /**
-     * 어플리케이션에서 사용할 redisTemplate 설정
-     * Json이 사용하기 편리하므로 ValueSerializer를 Jackson2JsonRedisSerializer로 등록한다.
-     */
+    /* 어플리케이션에서 사용할 redisTemplate 설정 */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
