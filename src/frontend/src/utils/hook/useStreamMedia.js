d7966e7e5ab0d000c0c3ec09c4a7c8e2d980b5a6
ag502 <ag502@naver.com>
2022-02-11 16:49:55 +0900

    fix : modify useStream docs

--- src/frontend/src/utils/hook/useStreamMedia.js
+++ src/frontend/src/utils/hook/useStreamMedia.js
@@ -18,6 +18,7 @@ export default function useStreamMedia(streamPlayerRef, device = 'web') {
 
   async function getMediaStream(facingMode = 'user') {
     try {
+      // 접속하는 디바이스가 모바일일 경우 전면 카메라로 방송시작
       if (device === 'mobile') {
         constraints = {
           audio: { echoCancellation: false },
