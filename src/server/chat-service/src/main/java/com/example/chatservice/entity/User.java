31b5ea0b1965b9a6c35ce01274b6ab72e8cbc44c
kimhanui <cocacola2017@naver.com>
2022-02-14 00:39:40 +0900

    feat : add User entity, repository

---
+++ src/server/chat-service/src/main/java/com/example/chatservice/entity/User.java
@@ -0,0 +1,78 @@
+package com.example.chatservice.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@NoArgsConstructor
+@Getter
+@Entity(name = "users")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 4444478977089006638L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(length = 36)
+    private String uuid;
+
+    @Column(length = 61)
+    private String pwd;
+
+    @Column(length = 30)
+    private String name;
+
+    @Column
+    private String nickName;
+
+    @Column(columnDefinition = "TEXT")
+    private String profileImage;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    private LocalDate createdAt;
+
+    @Column
+    private LocalDate modifiedAt;
+
+    @Column
+    private LocalDate deletedAt;
+
+    @Column
+    private String timeZone;
+
+    /**
+     * =============Friend=============
+     **/
+    @ManyToMany
+    @JoinTable(
+            name = "friend",
+            joinColumns = @JoinColumn(name = "users_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id")
+    )
+    private List<User> friends = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "friends") //나를 친구로 추가한 사람들
+    private List<User> beFriend = new ArrayList<>();
+    /**
+     * ================================
+     **/
+}
