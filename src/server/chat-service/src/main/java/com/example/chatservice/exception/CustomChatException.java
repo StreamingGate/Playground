215d8dbfaaeac21cb6ba6a13ca4eb1fafda12370
kimhanui <cocacola2017@naver.com>
2022-02-14 13:38:53 +0900

    feat : add CustomChatException.java, ExceptionController.java

--- src/server/chat-service/src/main/java/com/example/chatservice/exception/CustomChatException.java
+++ src/server/chat-service/src/main/java/com/example/chatservice/exception/CustomChatException.java
@@ -6,14 +6,14 @@ import lombok.Getter;
 public class CustomChatException extends RuntimeException {
 
     private ErrorCode errorCode;
-    private String roomId;
+    private String uuid; //room or user uuid
 
     public CustomChatException() {
         super();
     }
 
-    public CustomChatException(ErrorCode errorCode, String roomId) {
+    public CustomChatException(ErrorCode errorCode, String uuid) {
         this.errorCode = errorCode;
-        this.roomId = roomId;
+        this.uuid = uuid;
     }
 }
