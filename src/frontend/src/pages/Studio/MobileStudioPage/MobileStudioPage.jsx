c3cea1576f2554bbff7e3892dc55c7ef861d24a1
ag502 <ag502@naver.com>
2022-02-15 23:03:59 +0900

    feat : add streaming close logic in MobileStudioPage

--- src/frontend/src/pages/Studio/MobileStudioPage/MobileStudioPage.jsx
+++ src/frontend/src/pages/Studio/MobileStudioPage/MobileStudioPage.jsx
@@ -1,8 +1,11 @@
 import React, { useRef } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import * as S from './MobileStudioPage.style';
+import { modalService } from '@utils/service';
 import { useStreamMedia, useMediaSoupProduce } from '@utils/hook';
+
+import { AdviseModal } from '@components/feedbacks/Modals';
 
 import { IconButton } from '@components/buttons';
 
@@ -11,13 +14,15 @@ import { IconButton } from '@components/buttons';
  * @returns {React.Component} 실시간 방송 진행 페이지 모바일 웹뷰
  */
 function MobileStudioPage() {
+  const navigate = useNavigate();
+
   const streamPlayerRef = useRef(null);
 
   const { roomId, hostId } = useParams();
   // 모바일로 미디어 스트림 생성
   const { stream, switchCamera, stopStream } = useStreamMedia(streamPlayerRef, 'mobile');
 
-  const { producer } = useMediaSoupProduce(stream, roomId, hostId);
+  const { producer, newPeer } = useMediaSoupProduce(stream, roomId, hostId);
 
   // 전, 후면 카메라 전환 함수
   const handleCameraSwitchBtnClick = async () => {
@@ -26,7 +31,17 @@ function MobileStudioPage() {
 
   // 로컬 미디어 스트림 종료 함수
   const handleStopStreamBtnClick = () => {
-    stopStream();
+    modalService.show(AdviseModal, {
+      content: '방송을 종료하시겠습니까?',
+      type: 'cancel',
+      cancelBtnContent: '취소',
+      btnContent: '종료',
+      onClick: async () => {
+        stopStream();
+        await newPeer.request('closeProducer', { producerId: producer.id });
+        navigate('playground://producerClose');
+      },
+    });
   };
 
   return (
