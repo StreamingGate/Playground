45800e1e363b6e3b87a1d69b6e10824caa24adb2
ag502 <ag502@naver.com>
2022-02-15 17:26:18 +0900

    feat : connect with channel info API

--- src/frontend/src/pages/Channel/ChannelMetaData/ChannelMetaData.jsx
+++ src/frontend/src/pages/Channel/ChannelMetaData/ChannelMetaData.jsx
@@ -1,18 +1,24 @@
 import React, { memo } from 'react';
+import { useParams } from 'react-router-dom';
 
 import * as S from './ChannelMetaData.style';
+import { useGetChannelInfo } from '@utils/hook/query';
 
 function ChannelMetaData() {
+  const { id } = useParams();
+  const { data } = useGetChannelInfo(id);
+
   return (
     <S.ChannelMetaDataContainer>
-      <S.ChannelProfile size='xl' />
+      <S.ChannelProfile size='xl' imgSrc={data?.profileImage} />
       <S.ChannelInfo>
-        <S.ChannelName type='title'>채널이름</S.ChannelName>
+        <S.ChannelName type='title'>{data?.nickName}</S.ChannelName>
         <S.ChannelNumberData>
-          <S.ChannelFriendNum type='caption'>친구 2.01천명</S.ChannelFriendNum>
-          <S.ChannelVideoNum type='caption'>동영상 3개</S.ChannelVideoNum>
+          <S.ChannelFriendNum type='caption'>친구 {data?.friendCnt}명</S.ChannelFriendNum>
+          <S.ChannelVideoNum type='caption'>동영상 {data?.uploadCnt}개</S.ChannelVideoNum>
         </S.ChannelNumberData>
       </S.ChannelInfo>
+      {/* {userId !== data?.} */}
       <S.SubscribeBtn size='sm'>친구 신청</S.SubscribeBtn>
     </S.ChannelMetaDataContainer>
   );
