a073fedc83cd60dcc68652c31823d339f53b0b2b
kimhanui <cocacola2017@naver.com>
2022-02-16 12:47:44 +0900

    chore: remove installing iputils-ping in Dockerfile (main-service)

--- src/server/chat-service/src/main/java/com/example/chatservice/controller/RoomController.java
+++ src/server/chat-service/src/main/java/com/example/chatservice/controller/RoomController.java
@@ -1,11 +1,14 @@
 package com.example.chatservice.controller;
 
 import com.example.chatservice.dto.room.Room;
+import com.example.chatservice.dto.room.RoomResponseDto;
 import com.example.chatservice.redis.RedisRoomService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/chat")
@@ -15,13 +18,14 @@ public class RoomController {
     private final RedisRoomService redisRoomRepository;
 
     @GetMapping("/room/{roomUuid}")
-    public Room roomInfo(@PathVariable("roomUuid") String uuid) {
+    public RoomResponseDto roomInfo(@PathVariable("roomUuid") String uuid) {
         return redisRoomRepository.findById(uuid);
     }
 
     @PostMapping("/room")
-    public Room createRoom(@RequestBody Map<String, String> roomCreateDto) {
+    public RoomResponseDto createRoom(@RequestBody Map<String, String> roomCreateDto) {
         String uuid = roomCreateDto.get(UUID_KEY);
+        log.info("createRoom api: uuid=" + uuid);
         return redisRoomRepository.create(uuid);
     }
 
--- src/server/chat-service/src/main/java/com/example/chatservice/redis/RedisRoomService.java
+++ src/server/chat-service/src/main/java/com/example/chatservice/redis/RedisRoomService.java
@@ -4,6 +4,7 @@ import com.example.chatservice.dto.chat.ChatConsume;
 import com.example.chatservice.dto.chat.ChatProduce;
 import com.example.chatservice.dto.chat.SenderRole;
 import com.example.chatservice.dto.room.Room;
+import com.example.chatservice.dto.room.RoomResponseDto;
 import com.example.chatservice.exception.CustomChatException;
 import com.example.chatservice.exception.ErrorCode;
 import com.example.chatservice.utils.RedisMessaging;
@@ -40,14 +41,15 @@ public class RedisRoomService {
         return opsHashRoom.values(CHAT_ROOMS);
     }
 
-    public Room findById(String roomUuid) {
-        return opsHashRoom.get(CHAT_ROOMS, roomUuid);
+    public RoomResponseDto findById(String roomUuid) {
+        Room room = opsHashRoom.get(CHAT_ROOMS, roomUuid);
+        return new RoomResponseDto(room);
     }
 
-    public Room create(String roomUuid) {
+    public RoomResponseDto create(String roomUuid) {
         Room room = new Room(roomUuid);
         opsHashRoom.put(CHAT_ROOMS, room.getUuid(), room);
-        return room;
+        return new RoomResponseDto(room);
     }
 
     public void addPinnedChat(String roomUuid, ChatProduce pinnedChat) throws CustomChatException {
