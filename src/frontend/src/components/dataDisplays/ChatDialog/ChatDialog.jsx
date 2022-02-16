89b80e346dd3f0f78efc0c2409ead83bc153e7bf
ag502 <ag502@naver.com>
2022-02-16 11:41:02 +0900

    feat : apply time parse logic

--- src/frontend/src/components/dataDisplays/ChatDialog/ChatDialog.jsx
+++ src/frontend/src/components/dataDisplays/ChatDialog/ChatDialog.jsx
@@ -2,14 +2,15 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 
 import * as S from './ChatDialog.style';
+import { timeService } from '@utils/service';
 
 function ChatDialog({ chatInfo }) {
-  const { timeStamp, nickname, message, senderRole } = chatInfo;
+  const { timeStamp, nickname, message, senderRole, profileImage } = chatInfo;
   return (
     <S.ChatDialogContainer>
       <S.ChatMetaContainer>
-        <S.ChatProfile size='md' isAdmin={senderRole === 'STREAMER'} />
-        <S.TimeStamp type='bottomTab'>{timeStamp}</S.TimeStamp>
+        <S.ChatProfile size='md' isAdmin={senderRole === 'STREAMER'} imgSrc={profileImage} />
+        <S.TimeStamp type='bottomTab'>{timeService.processChatTime(timeStamp)}</S.TimeStamp>
         <S.UserName type='highlightCaption'>{nickname}</S.UserName>
       </S.ChatMetaContainer>
       <S.Message>{message}</S.Message>
