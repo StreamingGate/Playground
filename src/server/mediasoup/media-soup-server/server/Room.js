bea7b46348f5ceeb19bf67fe96ed4da1fc32974d
kmswlee <wodndl895@kookmin.ac.kr>
2022-02-16 11:00:39 +0900

    feat : worker load balancing and exit method on mediasoup

--- src/server/mediasoup/media-soup-server/server/Room.js
+++ src/server/mediasoup/media-soup-server/server/Room.js
@@ -1,6 +1,7 @@
 const protoo = require("protoo-server");
 const config = require("./config");
-
+const axios = require('axios');
+var EventEmitter = require('events').EventEmitter;
 const mediaCodecs = [
   {
     kind: "audio",
@@ -18,35 +19,45 @@ const mediaCodecs = [
   },
 ];
 
-class Room {
-  static async create({ mediasoupWorker, roomId }) {
+class Room extends EventEmitter {
+  static async create({ mediasoupWorker, roomId, workerNum }) {
     const protooRoom = new protoo.Room();
-    // const { mediaCodecs } = config.mediasoup.routerOptions;
     const mediasoupRouter = await mediasoupWorker.createRouter({ mediaCodecs });
     return new Room({
       roomId,
       protooRoom,
       mediasoupRouter,
+      workerNum
     });
   }
 
-  constructor({ roomId, protooRoom, mediasoupRouter }) {
+  constructor({ roomId, protooRoom, mediasoupRouter,workerNum }) {
+    super();
     this._roomId = roomId;
+    this._workerNum = workerNum;
     this._closed = false;
     this._protooRoom = protooRoom;
     this._mediasoupRouter = mediasoupRouter;
     this._producer = null;
+    this._audioProducer = null;
   }
   close() {
+
     this._closed = true;
 
-    // Close the protoo Room.
-    this._protooRoom.close();
-
     // Close the mediasoup Router.
+    console.log("미디어 라우터 삭제전~")
     this._mediasoupRouter.close();
+    console.log("미디어 라우터 삭제후~")
+
+    // Close the protoo Room.
+    console.log("프로투룸삭제전~")
+    this._protooRoom.close();
+    console.log("프로투룸삭제후~")
+
     // Emit 'close' event.
-    this.emit('close');
+    this.emit('close',this._workerNum);
+
   }
 
   handleProtooConnection({ peerId, consume, protooWebSocketTransport }) {
@@ -74,98 +85,111 @@ class Room {
     // 만들어진 peer에 이벤트 등록
     peer.on("request", (request, accept, reject) => {
       console.log(`protoo Peer request event ${request.method} ${peer.id}`);
- 
+
       this._handleProtooRequest(peer, request, accept, reject).catch(
-      (error) => {
-      console.log(`request failed ${error}`);
- 
-      reject(error);
-      }
-    );
+          (error) => {
+            console.log(`request failed ${error}`);
+
+            reject(error);
+          }
+      );
     });
   }
-  
+
   getRouterRtpCapabilities() {
     return this._mediasoupRouter.rtpCapabilities;
   }
 
   createWebRtcTransport = async () => {
     try {
+
       const webRtcTransport_options = {
-        listenIps: config.mediasoup.webRtcTransportOptions.listenIps,
+        ...config.mediasoup.webRtcTransportOptions,
         enableUdp: true,
         enableTcp: true,
         preferUdp: true,
       };
       let transport = await this._mediasoupRouter.createWebRtcTransport(
-        webRtcTransport_options
+          webRtcTransport_options
       );
- 
       transport.on("dtlsstatechange", (dtlsState) => {
-      if (dtlsState === "closed") {
-        transport.close();
-      }
+        if (dtlsState === "closed") {
+          transport.close();
+        }
       });
- 
       transport.on("close", () => {
         console.log("transport closed");
       });
- 
+
       const info = {
         id: transport.id,
         iceParameters: transport.iceParameters,
         iceCandidates: transport.iceCandidates,
         dtlsParameters: transport.dtlsParameters,
       };
+
       return [transport, info];
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
   };
+
   async _handleProtooRequest(peer, request, accept, reject) {
     switch (request.method) {
       case "getRouterRtpCapabilities": {
         accept(this._mediasoupRouter.rtpCapabilities);
         break;
-      }   
+      }
+      case "closeProducer": {
+        const { producerId } = request.data;
+        const producer = this._protooRoom.getPeer(producerId);
+        peer.notify('producerClose',
+            {
+              message: "방송종료하였습니다."
+            })
+        this.close();
+        accept();
+        break;
+      }
       case "createWebRtcTransport": {
         // producer
         if (!peer.data.consume) {
-          console.log("producer");
+          console.log("create produce webRtcTransport");
           const [transport, info] = await this.createWebRtcTransport();
           peer.data.produceTransport = transport;
           accept(info);
         } else {
           // consumer
-          console.log("consumer");
+          console.log("creat consume webRtcTransport");
+          console.log("this is flag")
           const [transport, info] = await this.createWebRtcTransport();
           peer.data.consumeTransport = transport;
           accept(info);
         }
         break;
-      }   
+      }
       case "produceConnect": {
         const { dtlsParameters } = request.data;
         if (peer.data?.produceTransport) {
-          await peer.data.produceTransport.connect({ dtlsParameters }); 
+          console.log("produceConnect");
+          await peer.data.produceTransport.connect({ dtlsParameters });
           accept(dtlsParameters);
         }
         break;
-      }   
+      }
       case "produceProduce": {
-        console.log("========produceProduce-===========")
         const { kind, rtpParameters, appData } = request.data;
         if (peer.data?.produceTransport) {
+          console.log("produceProduce");
           peer.data.producer = await peer.data.produceTransport.produce({
             kind,
             rtpParameters,
           });
           this._producer = peer.data.producer;
-          console.log(this._producer.id);
+          accept({ id: this._producer.id });
         }
-        accept({id: this._producer.id});
         break;
-      }   
+      }
       case "produceAudioProduce": {
         const { kind, rtpParameters, appData } = request.data;
         if (peer.data?.produceTransport) {
@@ -180,20 +204,21 @@ class Room {
         break;
       }
       case "connectConsume": {
-        console.log("hi");
+        //const { dtlsParameters } = request.data;
         const dtlsParameters = typeof request.data.dtlsParameters === 'object'?request.data.dtlsParameters:JSON.parse(request.data.dtlsParameters);
-        console.log(dtlsParameters);
         if (peer.data?.consumeTransport) {
+          console.log("connectConsume");
           try {
-            await peer.data.consumeTransport.connect({ dtlsParameters }); 
+            await peer.data.consumeTransport.connect({ dtlsParameters });
           } catch (error) {
             console.log(error.message);
           }
         }
-        accept({ id: "test" });
+        accept();
         break;
       }
       case "consume": {
+        // const { rtpCapabilities } = request.data;
         const rtpCapabilities = typeof request.data.rtpCapabilities === 'object'?request.data.rtpCapabilities:JSON.parse(request.data.rtpCapabilities);
         let canConsume = false;
         if (this._producer?.id) {
@@ -203,6 +228,7 @@ class Room {
             rtpCapabilities,
           });
         }
+
         try {
           if (canConsume && peer.data?.consumeTransport) {
             console.log("consume");
@@ -213,19 +239,43 @@ class Room {
             });
 
             peer.data.consumer.on("transportclose", () => {
+              console.log("transport close from consumer");
               peer.notify('producerClose',
-              {  
-                message: "방송종료하였습니다."
-              })
+                  {
+                    message: "방송종료하였습니다."
+                  })
               this.close();
             });
 
-            peer.data.consumer.on("producerclose", () => {
-              peer.notify('producerClose',
-              {  
-                message: "방송종료하였습니다."
-              })
+            peer.data.consumer.on("producerclose",  async (id) => {
+              console.log("producer of consumer closed");
+              await peer.notify('producerClose',
+                  {
+                    message: "방송종료하였습니다."
+                  })
               this.close();
+              console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!1')
+              const producerId = this._producer.id;
+              // try {
+              //   await axios.get(`http://localhost:8000/room-service`)
+
+              //   await axios.delete(`http://localhost:8000/room-service/room`, {
+              //     Headers: {
+              //       'Content-Type' : 'application/json',
+              //       'X-Requested-With': 'XMLHttpRequest'
+              //     },
+              //     data: {
+              //       roomId: 8,
+              //       hostUuid: "46e0e6b0-55a1-4eef-a2e0-3f8089e4596d"
+              //     }
+              //   }).then((response) => {
+              //     console.log("룸 삭제 성공")
+              //   })
+              // } catch(e) {
+              //   console.log(e);
+              // }
+              // this.close();
+              console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!2')
             });
 
             accept({
@@ -265,12 +315,13 @@ class Room {
             });
 
             peer.data.audioConsumer.on("transportclose", () => {
-              console.log("transport close from consumer");
+              console.log("transport close from consumer2");
             });
 
             peer.data.audioConsumer.on("producerclose", () => {
-              console.log("producer of consumer closed");
+              console.log("producer of consumer closed2");
             });
+
             accept({
               id: peer.data.audioConsumer.id,
               producerId: this._audioProducer.id,
--- src/server/mediasoup/media-soup-server/server/app.js
+++ src/server/mediasoup/media-soup-server/server/app.js
@@ -1,16 +1,18 @@
 const protoo = require("protoo-server");
 const mediasoup = require("mediasoup");
-const http = require("http");
-const expressApp = require("express");
+const https = require("http");
+const express = require("express");
 const url = require("url");
 const os = require("os");
+const fs = require("fs");
 const Room = require("./Room");
 const config = require("./config");
 const { AwaitQueue } = require("awaitqueue");
 const rooms = new Map();
+const roomCnt = new Map();
 const queue = new AwaitQueue();
-const mediasoupWorkers = [];
-let nextMediasoupWorkerIdx = 0;
+const mediasoupWorkerCnt = [];
+const mediasoupWorkers = new Map();
 run();
 
 async function run() {
@@ -20,34 +22,41 @@ async function run() {
 }
 
 async function runHttpServer() {
-  httpServer = http.createServer(expressApp);
+  expressApp = express();
+  const tls =
+      {
+        cert : fs.readFileSync(config.https.tls.cert),
+        key  : fs.readFileSync(config.https.tls.key)
+      };
+  httpsServer = https.createServer(expressApp);
 
-  httpServer.listen(4443, () => {
-    console.log("start");
+  await new Promise((resolve) =>
+  {
+    httpsServer.listen(
+        Number(config.https.listenPort), config.https.listenIp, resolve);
   });
 }
-
 async function runMediasoupWorkers() {
   const { numWorkers } = config.mediasoup;
 
   for (let i = 0; i < numWorkers; ++i) {
     const worker = await mediasoup.createWorker({
-    rtcMinPort: Number(config.mediasoup.workerSettings.rtcMinPort),
-    rtcMaxPort: Number(config.mediasoup.workerSettings.rtcMaxPort),
+      rtcMinPort: Number(config.mediasoup.workerSettings.rtcMinPort),
+      rtcMaxPort: Number(config.mediasoup.workerSettings.rtcMaxPort),
     });
 
     worker.on("died", () => {
       setTimeout(() => process.exit(1), 2000);
     });
-
-    mediasoupWorkers.push(worker);
+    mediasoupWorkers.set(i, worker);
+    mediasoupWorkerCnt.push({"num":i,"cnt":0});
   }
 }
 
 async function runProtooWebSocketServer() {
   console.log("running");
 
-  protooWebSocketServer = new protoo.WebSocketServer(httpServer, {
+  protooWebSocketServer = new protoo.WebSocketServer(httpsServer, {
     maxReceivedFrameSize: 960000, // 960 KBytes.
     maxReceivedMessageSize: 960000,
     fragmentOutgoingMessages: true,
@@ -68,61 +77,89 @@ async function runProtooWebSocketServer() {
       return;
     }
 
-    queue
-      .push(async () => {
-        const room = await getOrCreateRoom({ roomId });
-        
-        // Accept the protoo WebSocket connection.
-        const protooWebSocketTransport = accept();
-        
-        
 
-        // 일단 역할을 요청 url에 넣어둠
-        const consume = role === "produce" ? false : true;
-        room.handleProtooConnection({
-          peerId,
-          consume,
-          protooWebSocketTransport,
+    queue
+        .push(async () => {
+          const room = await CreateRoom({roomId});
+          // if(rooms.get(roomId) && roomCnt.get(roomId) > 320) {
+          //   room = await getOrCreateRoom({roomId});
+          // }
+          // else {
+          //   room = await CreateRoom({ roomId });
+          // }
+
+          // Accept the protoo WebSocket connection.
+          const protooWebSocketTransport = accept();
+
+          // 일단 역할을 요청 url에 넣어둠
+          const consume = role === "produce" ? false : true;
+
+          room.handleProtooConnection({
+            peerId,
+            consume,
+            protooWebSocketTransport,
+          });
+          console.log("방생성후 완료~~~~")
+        })
+        .catch((error) => {
+          reject(error);
         });
-        console.log(room);
-      })
-      .catch((error) => {
-        reject(error);
-      });
   });
 }
 
 function getMediasoupWorker() {
-  const worker = mediasoupWorkers[nextMediasoupWorkerIdx];
 
-  if (++nextMediasoupWorkerIdx === mediasoupWorkers.length)
-    nextMediasoupWorkerIdx = 0;
+  let minWorker = mediasoupWorkerCnt.sort(function(a,b) {
+    return a["cnt"] - b["cnt"];
+  })[0];
 
-  return worker;
+  const worker = mediasoupWorkers.get(minWorker["num"]);
+  minWorker["cnt"]++;
+
+  return [worker,minWorker["num"]];
 }
 
-async function getOrCreateRoom({ roomId }) {
+async function CreateRoom({ roomId }) {
   let room = rooms.get(roomId);
-  console.log(room.peers.length)
-  // if(room && )
 
   // If the Room does not exist create a new one.
   if (!room) {
-    console.log("here");
+    console.log("룸생성 시작");
 
-    const mediasoupWorker = getMediasoupWorker();
+    const [mediasoupWorker,workerNum] = getMediasoupWorker();
 
     try {
-      room = await Room.create({ mediasoupWorker, roomId });
+      room = await Room.create({ mediasoupWorker, roomId, workerNum });
     } catch (error) {
       console.log(error.message);
     }
     rooms.set(roomId, room);
-    // event emitter 상속후 가능?
+
+    // event emitter 상속후 가능
     room.on("close", () => {
-        rooms.delete(roomId);
+      console.log("룸이 클로즈 됐습니다. 제발 여기 걸려라")
+
+      const minWorker = mediasoupWorkerCnt.find((obj) => {
+        return obj["num"] === workerNum;
+      });
+      minWorker["cnt"]--;
+      rooms.delete(roomId);
     });
   }
 
   return room;
 }
+
+// async function getOrCreateRoom({roomId}) {
+//   let room = rooms.get(roomId).getLast();
+//   if(room && !room.hasPeer(peerId) && roomCnt.get(roomId) < 320) {
+//     roomCnt.set(roomId,roomCnt.get(roomId)+1);
+//     return room;
+//   }
+//   else if(room && roomCnt.get(roomId) > 320) {
+//     const mediasoupWorker = getMediasoupWorker();
+//     let connectRoom = await Room.create({room,mediasoupWorker,roomId});
+//     rooms.get(roomId).push(connectRoom);
+//     return connectRoom;
+//   }
+// }
