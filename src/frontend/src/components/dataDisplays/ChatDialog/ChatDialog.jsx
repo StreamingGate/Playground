9fc25b242b7b416719a782557345f19dc58e6ad3
ag502 <ag502@naver.com>
2022-02-16 14:02:08 +0900

    design : add pinned chat dialog style

--- src/frontend/src/components/dataDisplays/ChatDialog/ChatDialog.jsx
+++ src/frontend/src/components/dataDisplays/ChatDialog/ChatDialog.jsx
@@ -4,13 +4,16 @@ import PropTypes from 'prop-types';
 import * as S from './ChatDialog.style';
 import { timeService } from '@utils/service';
 
-function ChatDialog({ chatInfo }) {
+function ChatDialog({ chatInfo, isPinned }) {
   const { timeStamp, nickname, message, senderRole, profileImage } = chatInfo;
   return (
     <S.ChatDialogContainer>
       <S.ChatMetaContainer>
         <S.ChatProfile size='md' isAdmin={senderRole === 'STREAMER'} imgSrc={profileImage} />
-        <S.TimeStamp type='bottomTab'>{timeService.processChatTime(timeStamp)}</S.TimeStamp>
+        {isPinned && <S.PinText type='highlightCaption'>상단고정</S.PinText>}
+        {!isPinned && (
+          <S.TimeStamp type='bottomTab'>{timeService.processChatTime(timeStamp)}</S.TimeStamp>
+        )}
         <S.UserName type='highlightCaption'>{nickname}</S.UserName>
       </S.ChatMetaContainer>
       <S.Message>{message}</S.Message>
@@ -26,6 +29,11 @@ ChatDialog.propTypes = {
     message: PropTypes.string,
     senderRole: PropTypes.oneOf(['STREAMER', 'VIEWER']),
   }).isRequired,
+  isPinned: PropTypes.bool,
+};
+
+ChatDialog.defaultProps = {
+  isPinned: false,
 };
 
 export default memo(ChatDialog);
