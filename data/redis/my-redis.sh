dea0f3449049e492bf2bdf5511b5db478a622e11
kimhanui <cocacola2017@naver.com>
2022-02-17 19:34:12 +0900

    feat : init data of redis
    -  유저 4명 포함

---
+++ data/redis/my-redis.sh
@@ -0,0 +1,10 @@
+# start server in background and wait for 1 sec
+redis-server --daemonize yes && sleep 1
+# slurp all data from file to redis in memory db (note the dir)
+redis-cli < /my-dir/my-data.redis
+# persist data to disk
+redis-cli save
+# stop background server
+redis-cli shutdown
+# start the server normally
+redis-server
