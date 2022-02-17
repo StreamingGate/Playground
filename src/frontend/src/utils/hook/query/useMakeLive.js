350b8dbabcf37a595154f6546f6fe142ce61b5b5
ag502 <ag502@naver.com>
2022-02-18 00:25:16 +0900

    feat : apply toast message component in stream live

--- src/frontend/src/utils/hook/query/useMakeLive.js
+++ src/frontend/src/utils/hook/query/useMakeLive.js
@@ -1,6 +1,8 @@
 import { useMutation } from 'react-query';
 import axios from '@utils/axios';
 
+import { toast } from 'react-toastify';
+
 const postMakeLive = async body => {
   const data = await axios.post('/room-service/room', body);
   return data;
@@ -8,6 +10,23 @@ const postMakeLive = async body => {
 
 export default function useMakeLive(handleMakeLiveSuccess) {
   return useMutation(body => postMakeLive(body), {
-    onSuccess: handleMakeLiveSuccess,
+    onMutate: () => {
+      toast.dismiss();
+      toast.info('실시간 스트리밍 시작 중...', {
+        autoClose: false,
+        position: 'top-right',
+      });
+    },
+    onSuccess: async data => {
+      toast.dismiss();
+      await handleMakeLiveSuccess(data);
+    },
+    onError: () => {
+      toast.dismiss();
+      toast.warn('실시간 스트리밍 생성에 실패했습니다. 다시 시도해 주세요', {
+        autoClose: false,
+        position: 'top-right',
+      });
+    },
   });
 }
