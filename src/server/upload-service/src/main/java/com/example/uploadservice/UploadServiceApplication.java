1baff5fbd612f87e4db05e67f792272aecbf899e
kimhanui <cocacola2017@naver.com>
2022-02-17 22:36:21 +0900

    chore : remove eureka service

--- src/server/upload-service/src/main/java/com/example/uploadservice/UploadServiceApplication.java
+++ src/server/upload-service/src/main/java/com/example/uploadservice/UploadServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.uploadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UploadServiceApplication {
 
     public static void main(String[] args) {
