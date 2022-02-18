b5aaf8dfe9220eca6e54cda46e783bea5d7c1806
chaehui seo <sch0991@naver.com>
2022-02-18 11:46:49 +0900

    design : add ui event for auto login

--- src/ios/PlayGround/PlayGround/Network/MainService/LoadingView.swift
+++ src/ios/PlayGround/PlayGround/Network/MainService/LoadingView.swift
@@ -27,4 +27,26 @@ extension AnimationView {
         self.removeFromSuperview()
         backView.removeFromSuperview()
     }
+    
+    func setAutoLoginLoading(vc: UIViewController, backView: UIView) {
+        backView.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+        vc.view.addSubview(backView)
+        vc.view.addSubview(self)
+        let autoLoginTryingLabel = UILabel()
+        backView.addSubview(autoLoginTryingLabel)
+        autoLoginTryingLabel.text = "자동로그인 중..."
+        autoLoginTryingLabel.textColor = UIColor.PGBlue
+        autoLoginTryingLabel.font = UIFont.Tab
+        autoLoginTryingLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            autoLoginTryingLabel.topAnchor.constraint(equalTo: self.bottomAnchor, constant: 10),
+            autoLoginTryingLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+        ])
+        backView.frame = vc.view.frame
+        self.frame = CGRect(x: vc.view.bounds.midX - 50, y: vc.view.bounds.midY - 100, width: 100, height: 100)
+        self.contentMode = .scaleAspectFit
+        self.animationSpeed = 2
+        self.play()
+        self.loopMode = .loop
+    }
 }
