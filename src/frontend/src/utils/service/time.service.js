0b47807fb0b412b338e548701ddc27dc098dcb38
ag502 <ag502@naver.com>
2022-02-19 21:34:22 +0900

    feat : implement time logic

--- src/frontend/src/utils/service/time.service.js
+++ src/frontend/src/utils/service/time.service.js
@@ -6,29 +6,18 @@ const processVideoUploadTime = time => {
     uploadTime.getTime() - uploadTime.getTimezoneOffset() * 60 * 1000
   );
 
-  const seconds = Math.floor((now - convertedUploadTime) / 1000);
-  let difference = seconds / 31536000;
+  const diff = Math.floor((now.getTime() - convertedUploadTime.getTime()) / 60000);
 
-  if (difference > 1) {
-    return `${Math.floor(difference)}년 전`;
+  let convertDay = '';
+
+  if (diff < 60) {
+    convertDay = `${diff}분 전`;
+  } else if (diff <= 1440) {
+    convertDay = `${Math.floor(diff / 60)}시간 전`;
+  } else {
+    convertDay = `${diff / 1440}일 전`;
   }
-
-  difference = seconds / 2592000;
-  if (difference > 1) {
-    return `${Math.floor(difference)}개월 전`;
-  }
-
-  difference = seconds / 86400;
-  if (difference > 1) {
-    return `${Math.floor(difference)}일 전`;
-  }
-
-  difference = seconds / 3600;
-  if (difference > 1) {
-    return `${Math.floor(difference)}시간 전`;
-  }
-
-  return `${Math.floor(difference)}분 전`;
+  return convertDay;
 };
 
 const processChatTime = time => {
