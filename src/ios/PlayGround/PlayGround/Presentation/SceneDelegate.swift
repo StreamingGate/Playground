7b09cc6536da9dcdce9b4cf5f927add5638d2ebb
Chaehui Seo <sch0991@naver.com>
2022-03-05 22:41:11 +0900

    comment : delete comments in sceneDelegate

--- src/ios/PlayGround/PlayGround/Presentation/SceneDelegate.swift
+++ src/ios/PlayGround/PlayGround/Presentation/SceneDelegate.swift
@@ -21,10 +21,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
         print("disconnected")
         guard let root = window?.rootViewController as? LoginNavigationController, let tabBar = root.viewControllers.last as? CustomTabViewController else { return }
         if let player = tabBar.children.last as? PlayViewController, let chatting = player.children.last(where: { ($0 as? ChattingViewController) != nil }) as? ChattingViewController {
@@ -38,8 +34,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
         StatusServiceAPI.shared.getFriendInfo { result in
             guard let friends = result["data"] as? FriendWatchList else { return }
             StatusManager.shared.friendWatchList = friends.result
@@ -60,9 +54,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidEnterBackground(_ scene: UIScene) {
         print("background")
         StatusManager.shared.disconnectToSocket()
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
     }
 
 
